b0e6fd5601f70ce20d00a67458c4b6632184bdd4
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2014-12-13 00:58:28 -0600

    reverse lineage

--- builder/lineage_hunter.js
+++ builder/lineage_hunter.js
@@ -17,30 +17,45 @@
 
 			pattern.lineage = [];
 			pattern.lineageIndex = [];
+			pattern.lineageR = [];
+			pattern.lineageRIndex = [];
 			//find the {{> template-name }} within patterns
 			var matches = pattern.template.match(/{{>([ ]+)?([A-Za-z0-9-]+)(?:\:[A-Za-z0-9-]+)?(?:(| )\(.*)?([ ]+)}}/g);
 			if(matches !== null){
 				matches.forEach(function(match, index, matches){
 					//strip out the template cruft
-					var cleanPattern = match.replace("{{> ", "").replace(" }}", "");
+					var foundPattern = match.replace("{{> ", "").replace(" }}", "");
 
 					//add if it doesnt exist
-					if (pattern.lineageIndex.indexOf(cleanPattern) === -1){
+					if (pattern.lineageIndex.indexOf(foundPattern) === -1){
 
-						pattern.lineageIndex.push(cleanPattern);
+						pattern.lineageIndex.push(foundPattern);
 
-						patternlab.patterns.forEach(function(p, index, patterns){
+						patternlab.patterns.forEach(function(ancestorPattern, index, patterns){
 
 							//find the pattern in question
-							var searchPattern = p.patternGroup + "-" + p.patternName;
+							var searchPattern = ancestorPattern.patternGroup + "-" + ancestorPattern.patternName;
 
-							if(searchPattern === cleanPattern){
+							if(searchPattern === foundPattern){
 								//create the more complex patternLineage object too
 								var l = {
-									"lineagePattern": cleanPattern,
-									"lineagePath": "../../patterns/" + p.patternLink
-								}
+									"lineagePattern": foundPattern,
+									"lineagePath": "../../patterns/" + ancestorPattern.patternLink
+								};
 								pattern.lineage.push(JSON.stringify(l));
+
+								//also, add the lineageR entry if it doesn't exist
+								var patternLabel = pattern.patternGroup + "-" + pattern.patternName;
+								if (ancestorPattern.lineageRIndex.indexOf(patternLabel) === -1){
+									ancestorPattern.lineageRIndex.push(patternLabel);
+
+									//create the more complex patternLineage object in reverse
+									var lr = {
+										"lineagePattern": patternLabel,
+										"lineagePath": "../../patterns/" + pattern.patternLink
+									};
+									ancestorPattern.lineageR.push(JSON.stringify(lr));
+								}
 							}
 
 						});
