38c12c613ea127f37be30f391f4aaf82e2e58b63
Brian Muenzenmeyer <bmuenzenmeyer@sbcglobal.net>
2015-02-11 01:17:19 -0600

    Merge pull request #96 from pattern-lab/dev
    Merging changes for patternlab 0.8.0

--- packages/patternengine-node-mustache/builder/object_factory.js
+++ packages/patternengine-node-mustache/builder/object_factory.js
@@ -1,5 +1,5 @@
 /* 
- * patternlab-node - v0.1.6 - 2014 
+ * patternlab-node - v0.8.0 - 2015 
  * 
  * Brian Muenzenmeyer, and the web community.
  * Licensed under the MIT license. 
@@ -11,18 +11,19 @@
 (function () {
 	"use strict";
 
-	var oPattern = function(name, subdir, filename, data){
-		this.name = name; //this is the unique name with the subDir
+	var oPattern = function(subdir, filename, data){
+		this.fileName = filename.substring(0, filename.indexOf('.'));
 		this.subdir = subdir;
-		this.filename = filename;
-		this.data =  data;
-		this.template = '';
-		this.patternPartial = '';
-		this.patternName = ''; //this is the display name for the ui
-		this.patternLink = '';
-		this.patternGroup = name.substring(name.indexOf('-') + 1, name.indexOf('-', 4) + 1 - name.indexOf('-') + 1);
+		this.name = (subdir.replace(/[\/\\]/g, '-') + '-' + this.fileName).replace(/\\/g, '-'); //this is the unique name with the subDir
+		this.data =  data || null;
+		this.patternName = this.fileName.substring(this.fileName.indexOf('-') + 1); //this is the display name for the ui
+		this.patternLink = this.name + '/' + this.name + '.html';
+		this.patternGroup = this.name.substring(this.name.indexOf('-') + 1, this.name.indexOf('-', 4) + 1 - this.name.indexOf('-') + 1);
 		this.patternSubGroup = subdir.substring(subdir.indexOf('/') + 4);
 		this.flatPatternPath = subdir.replace(/\//g, '-');
+		this.key = this.patternGroup + '-' + this.patternName;
+		this.template = '';
+		this.patternPartial = '';
 		this.lineage = [];
 		this.lineageIndex = [];
 		this.lineageR = [];
--- packages/patternengine-node-mustache/builder/pattern_assembler.js
+++ packages/patternengine-node-mustache/builder/pattern_assembler.js
@@ -1,5 +1,35 @@
 (function () {
-	"use strict";
-	
+  "use strict";
+
+  var fs = require('fs-extra'),
+      path = require('path');
+
+  var pattern_assembler = function(){
+
+    function setState(pattern, patternlab){
+      if(patternlab.config.patternStates[pattern.patternName]){
+        pattern.patternState = patternlab.config.patternStates[pattern.patternName];
+      } else{
+        pattern.patternState = "";
+      }
+    }
+
+    function addPattern(pattern, patternLab){
+      patternLab.data.link[pattern.patternGroup + '-' + pattern.patternName] = '/patterns/' + pattern.patternLink;
+      patternLab.patterns.push(pattern);
+    }
+
+    return {
+      setPatternState: function(pattern, patternlab){
+        setState(pattern, patternlab);
+      },
+      addPattern: function(pattern, patternLab){
+        addPattern(pattern, patternLab);
+      }
+    };
+
+  };
+
+  module.exports = pattern_assembler;
 
 }());
