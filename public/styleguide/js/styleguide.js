fa894385c635f7b9a75d90530a359a38d413d6e3
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2015-03-09 22:24:01 -0500

    Update styleguide accordion per pl php https://raw.githubusercontent.com/pattern-lab/patternlab-php/master/core/styleguide/js/styleguide.js now will close open accordions when you click a different nav menu which closes #104

--- public/styleguide/js/styleguide.js
+++ public/styleguide/js/styleguide.js
@@ -19,16 +19,39 @@
 	$(w).resize(function(){ //Update dimensions on resize
 		sw = document.body.clientWidth;
 		sh = document.body.clientHeight;
+
+		setAccordionHeight();
 	});
 
 	/* Pattern Lab accordion dropdown */
+	// Accordion dropdown
 	$('.sg-acc-handle').on("click", function(e){
-		var $this = $(this),
-			$panel = $this.next('.sg-acc-panel');
 		e.preventDefault();
+
+		var $this = $(this),
+			$panel = $this.next('.sg-acc-panel'),
+			subnav = $this.parent().parent().hasClass('sg-acc-panel');
+
+		//Close other panels if link isn't a subnavigation item
+		if (!subnav) {
+			$('.sg-acc-handle').not($this).removeClass('active');
+			$('.sg-acc-panel').not($panel).removeClass('active');
+		}
+
+		//Activate selected panel
 		$this.toggleClass('active');
 		$panel.toggleClass('active');
+		setAccordionHeight();
 	});
+
+	//Accordion Height 
+	function setAccordionHeight() {
+		var $activeAccordion = $('.sg-acc-panel.active').first(),
+			accordionHeight = $activeAccordion.height(),
+			availableHeight = sh-$headerHeight; //Screen height minus the height of the header
+		
+		$activeAccordion.height(availableHeight); //Set height of accordion to the available height
+	}
 
 	$('.sg-nav-toggle').on("click", function(e){
 		e.preventDefault();
