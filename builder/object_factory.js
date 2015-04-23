726c18ce1d2c0512333b5d70cd5ee5dc76718caf
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2015-04-22 23:23:03 -0500

    cleaned up some whitespace. will try to slowly convert over to using spaces instead of tabs for indentation updated CHANGELOG

--- builder/object_factory.js
+++ builder/object_factory.js
@@ -9,54 +9,54 @@
  */
 
 (function () {
-	"use strict";
+  "use strict";
 
-	var oPattern = function(subdir, filename, data){
-		this.fileName = filename.substring(0, filename.indexOf('.'));
-		this.subdir = subdir;
-        this.name = subdir.replace(/[\/\\]/g, '-') + '-' + this.fileName; //this is the unique name with the subDir
-		this.data =  data || null;
-		this.patternName = this.fileName.substring(this.fileName.indexOf('-') + 1); //this is the display name for the ui
-		this.patternLink = this.name + '/' + this.name + '.html';
-		this.patternGroup = this.name.substring(this.name.indexOf('-') + 1, this.name.indexOf('-', 4) + 1 - this.name.indexOf('-') + 1);
-		this.patternSubGroup = subdir.substring(subdir.indexOf('/') + 4);
-        this.flatPatternPath = subdir.replace(/[\/\\]/g, '-');
-		this.key = this.patternGroup + '-' + this.patternName;
-		this.template = '';
-		this.patternPartial = '';
-		this.lineage = [];
-		this.lineageIndex = [];
-		this.lineageR = [];
-		this.lineageRIndex = [];
-	};
+  var oPattern = function(subdir, filename, data){
+    this.fileName = filename.substring(0, filename.indexOf('.'));
+    this.subdir = subdir;
+    this.name = subdir.replace(/[\/\\]/g, '-') + '-' + this.fileName; //this is the unique name with the subDir
+    this.data =  data || null;
+    this.patternName = this.fileName.substring(this.fileName.indexOf('-') + 1); //this is the display name for the ui
+    this.patternLink = this.name + '/' + this.name + '.html';
+    this.patternGroup = this.name.substring(this.name.indexOf('-') + 1, this.name.indexOf('-', 4) + 1 - this.name.indexOf('-') + 1);
+    this.patternSubGroup = subdir.substring(subdir.indexOf('/') + 4);
+    this.flatPatternPath = subdir.replace(/[\/\\]/g, '-');
+    this.key = this.patternGroup + '-' + this.patternName;
+    this.template = '';
+    this.patternPartial = '';
+    this.lineage = [];
+    this.lineageIndex = [];
+    this.lineageR = [];
+    this.lineageRIndex = [];
+  };
 
-	var oBucket = function(name){
-		this.bucketNameLC = name;
-		this.bucketNameUC = name.charAt(0).toUpperCase() + name.slice(1);
-		this.navItems = [];
-		this.navItemsIndex = [];
-		this.patternItems = [];
-		this.patternItemsIndex = [];
-	};
+  var oBucket = function(name){
+    this.bucketNameLC = name;
+    this.bucketNameUC = name.charAt(0).toUpperCase() + name.slice(1);
+    this.navItems = [];
+    this.navItemsIndex = [];
+    this.patternItems = [];
+    this.patternItemsIndex = [];
+  };
 
-	var oNavItem = function(name){
-		this.sectionNameLC = name;
-		this.sectionNameUC = name.charAt(0).toUpperCase() + name.slice(1);
-		this.navSubItems = [];
-		this.navSubItemsIndex = [];
-	};
+  var oNavItem = function(name){
+    this.sectionNameLC = name;
+    this.sectionNameUC = name.charAt(0).toUpperCase() + name.slice(1);
+    this.navSubItems = [];
+    this.navSubItemsIndex = [];
+  };
 
-	var oNavSubItem = function(name){
-		this.patternPath = '';
-		this.patternPartial = '';
-		this.patternName = name.charAt(0).toUpperCase() + name.slice(1);
-	};
+  var oNavSubItem = function(name){
+    this.patternPath = '';
+    this.patternPartial = '';
+    this.patternName = name.charAt(0).toUpperCase() + name.slice(1);
+  };
 
-	module.exports = {
-		oPattern: oPattern,
-		oBucket: oBucket,
-		oNavItem: oNavItem,
-		oNavSubItem: oNavSubItem
-	};
+  module.exports = {
+    oPattern: oPattern,
+    oBucket: oBucket,
+    oNavItem: oNavItem,
+    oNavSubItem: oNavSubItem
+  };
 
 }());
