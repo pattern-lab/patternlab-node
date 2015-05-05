3ba0983325810281450c65adc3e146e42d98f3d4
Brad Frost <frostbd@gmail.com>
2015-05-04 21:40:17 -0400

    Code view tabs

--- packages/uikit-default/dist/js/patternlab-pattern.js
+++ packages/uikit-default/dist/js/patternlab-pattern.js
@@ -568,3 +568,20 @@ for (var i = 0; i < els.length; ++i) {
 		};
 	})(item);
 }
+
+
+// Code view tabs
+var tabLink = document.querySelectorAll(".sg-tab-list a"),
+	els = tabLink;
+
+for (var i = 0; i < els.length; ++i) {
+	els[i].onclick = (function(item) {
+		return function(e) {	
+			e.preventDefault();
+			this.classList.add("active");
+			var thisHref = this.getAttribute("href");
+			document.querySelector(thisHref).classList.add("active");
+			// Add class to tab panel. Not working for some weird fucking reason 
+		};
+	})(item);
+}
