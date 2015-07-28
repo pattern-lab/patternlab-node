977983cb50cc861c923442798dddad3bbfa36152
Scott Nath <github@scottnath.com>
2015-07-28 12:47:58 -0400

    view all pages include baseurl now

--- builder/patternlab.js
+++ builder/patternlab.js
@@ -204,13 +204,18 @@ var entity_encoder = new he();
     patternlab.patternPaths = {};
     patternlab.viewAllPaths = {};
 
+    var baseurl;
+    // check for baseurl in config.json
+    if(patternlab.config.baseurl){
+      baseurl = patternlab.config.baseurl;
+    }
     //find mediaQueries
     var media_hunter = new mh();
     media_hunter.find_media_queries(patternlab);
 
     //build the styleguide
     var styleguideTemplate = fs.readFileSync('./source/_patternlab-files/styleguide.mustache', 'utf8');
-    var styleguideHtml = renderPattern(styleguideTemplate, {partials: patternlab.patterns});
+    var styleguideHtml = renderPattern(styleguideTemplate, {partials: patternlab.patterns, baseurl: baseurl});
     fs.outputFileSync('./public/styleguide/html/styleguide.html', styleguideHtml);
 
     //build the viewall pages
@@ -235,7 +240,7 @@ var entity_encoder = new he();
         }
 
         var viewAllTemplate = fs.readFileSync('./source/_patternlab-files/viewall.mustache', 'utf8');
-        var viewAllHtml = renderPattern(viewAllTemplate, {partials: viewAllPatterns, patternPartial: patternPartial});
+        var viewAllHtml = renderPattern(viewAllTemplate, {partials: viewAllPatterns, patternPartial: patternPartial, baseurl: baseurl});
         fs.outputFileSync('./public/patterns/' + pattern.flatPatternPath + '/index.html', viewAllHtml);
       }
     }
