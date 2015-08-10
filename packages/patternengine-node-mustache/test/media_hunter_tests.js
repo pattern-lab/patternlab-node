30373e7553cd5653ebb41a6d11f0b960e1ac7094
Brian Muenzenmeyer <bmuenzenmeyer@sbcglobal.net>
2015-08-09 22:35:05 -0500

    Merge pull request #137 from pattern-lab/dev
    v0.10.1

--- packages/patternengine-node-mustache/test/media_hunter_tests.js
+++ packages/patternengine-node-mustache/test/media_hunter_tests.js
@@ -3,9 +3,6 @@
 
 	var MediaHunter = require('../builder/media_hunter');
 
-	//all tests here run against the shipped code, and as such altering any of that can break these tests
-	//these tests are intended to catch development regressions. one should not have a need to run tests during patternlab use. 
-
 	exports['media_query hunter basic support '] = {
 		'test media hunter finds query' : function(test){
 
@@ -13,49 +10,62 @@
 			var pl = {};
 			var mh = new MediaHunter();
 
-			mh.find_media_queries(pl);
+			mh.find_media_queries('./test/files', pl);
 
-			test.equals(pl.mediaQueries.length, 7);
+			test.equals(pl.mediaQueries.length, 4);
 
 			test.done();
-		}
-	};
+		},
 
-	exports['media_query hunter spaces '] = {
 		'test media hunter supports spaces in media query' : function(test){
 
 			//setup pl object
 			var pl = {};
 			var mh = new MediaHunter();
 
-			mh.find_media_queries(pl);
+			mh.find_media_queries('./test/files', pl);
 
-			test.equals(pl.mediaQueries[0], '24em');
+			test.equals(pl.mediaQueries[0], '35em');
 
 			test.done();
-		}
-	};
+		},
 
-	exports['media_query hunter no spaces '] = {
 		'test media hunter supports nospaces in media query' : function(test){
 
 			//setup pl object
 			var pl = {};
 			var mh = new MediaHunter();
 
-			mh.find_media_queries(pl);
+			mh.find_media_queries('./test/files', pl);
 
-			//the last media query found in the shipped suite does not have a space
-			//you can see this here:
+			test.equals(pl.mediaQueries[1], '50em');
 
-			//./source/css/scss/base/_global-classes.scss
-			// .hide-large-2 {
-			//     @media all and (min-width:$bp-large-2) {
-			//         display: none;
-			//     }
-			// }
- 
-			test.equals(pl.mediaQueries[6], '66em');
+			test.done();
+		},
+
+		'calling media hunter twice does not double add media queries' : function(test){
+
+			//setup pl object
+			var pl = {};
+			var mh = new MediaHunter();
+
+			mh.find_media_queries('./test/files', pl);
+			mh.find_media_queries('./test/files', pl);
+
+			test.equals(pl.mediaQueries.length, 4);
+
+			test.done();
+		},
+
+		'encountering the same breakpoint in a file does not double add' : function(test){
+
+			//setup pl object
+			var pl = {};
+			var mh = new MediaHunter();
+
+			mh.find_media_queries('./test/files', pl);
+
+			test.equals(pl.mediaQueries.length, 4);
 
 			test.done();
 		}
