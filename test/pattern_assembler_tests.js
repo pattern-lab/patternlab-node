9f510b6123feaeda8d4295f587bd74e02321debf
e2tha-e <hyunhoee@gmail.com>
2015-09-11 16:46:14 -0400

    unit tests for verbose mustache partials

--- test/pattern_assembler_tests.js
+++ test/pattern_assembler_tests.js
@@ -19,6 +19,23 @@
 			test.equals(results[1], '{{> molecules-single-comment(description: \'A life is like a garden. Perfect moments can be had, but not preserved, except in memory.\') }}');
 
 			test.done();
+		},
+
+		'find_pattern_partials finds verbose partials' : function(test){
+
+			//setup current pattern from what we would have during execution
+			var currentPattern = {
+				"template": "<h1>{{> 01-molecules/06-components/03-comment-header.mustache }}</h1><div>{{> 01-molecules/06-components/02-single-comment(description: 'A life is like a garden. Perfect moments can be had, but not preserved, except in memory.') }}</div>",
+			};
+
+			var pattern_assembler = new pa();
+
+			var results = pattern_assembler.find_pattern_partials(currentPattern);
+			test.equals(results.length, 2);
+			test.equals(results[0], '{{> 01-molecules/06-components/03-comment-header.mustache }}');
+			test.equals(results[1], '{{> 01-molecules/06-components/02-single-comment(description: \'A life is like a garden. Perfect moments can be had, but not preserved, except in memory.\') }}');
+
+			test.done();
 		}
 
 	};
