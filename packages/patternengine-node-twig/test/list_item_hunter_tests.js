b094febb91825480865522a538ffafbe3f4d46e9
Brian Muenzenmeyer <bmuenzenmeyer@sbcglobal.net>
2015-10-06 00:48:10 -0500

    Merge pull request #161 from pattern-lab/dev
    Dev push for v0.13.0

--- packages/patternengine-node-twig/test/list_item_hunter_tests.js
+++ packages/patternengine-node-twig/test/list_item_hunter_tests.js
@@ -100,7 +100,7 @@
       test.done();
     },
 
-    'process_list_item_partials overwrites listItem data if local .listitem.json is found' : function(test){
+    'process_list_item_partials overwrites listItem property if that property is in local .listitem.json' : function(test){
       //arrange
       //setup current pattern from what we would have during execution
       var currentPattern = {
@@ -117,7 +117,7 @@
                       "title": "Two"
                    },
                 ]
-          }         
+          }
       };
 
       var patternlab = {
@@ -155,9 +155,133 @@
 
       //act
       list_item_hunter.process_list_item_partials(currentPattern, patternlab);
-      
+
       //assert
       test.equals(currentPattern.extendedTemplate, "OneTwo" );
+
+      test.done();
+    },
+
+    'process_list_item_partials keeps listItem property if that property is not in local .listitem.json' : function(test){
+      //arrange
+      //setup current pattern from what we would have during execution
+      var currentPattern = {
+         "template": "{{#listItems.one}}{{ title }}{{/listItems.one}}",
+         "extendedTemplate" : "{{#listItems.one}}{{> test-simple }}{{/listItems.one}}",
+         "key": "test-patternName",
+         "jsonFileData" : {},
+         "patternSpecificListJson" : {
+            "2": [
+                   {
+                      "title": "One"
+                   },
+                   {
+                      "title": "Two"
+                   },
+                ]
+          }
+      };
+
+      var patternlab = {
+        "listitems": {
+          "1": [
+             {
+                "title": "Foo"
+             }
+          ],
+          "2": [
+             {
+                "title": "Foo"
+             },
+             {
+                "title": "Bar"
+             }
+          ]
+        },
+        "data": {
+          "link": {},
+          "partials": []
+        },
+        "config": {"debug": false},
+        "patterns": [
+          {
+           "template": "{{ title }}",
+           "extendedTemplate" : "{{ title }}",
+           "key": "test-simple",
+           "jsonFileData" : {}
+          }
+        ]
+      };
+
+      var list_item_hunter = new lih();
+
+      //act
+      list_item_hunter.process_list_item_partials(currentPattern, patternlab);
+
+      //assert
+      test.equals(currentPattern.extendedTemplate, "Foo" );
+
+      test.done();
+    },
+
+    'process_list_item_partials uses local listItem property if that property is not set globally' : function(test){
+      //arrange
+      //setup current pattern from what we would have during execution
+      var currentPattern = {
+         "template": "{{#listItems.one}}{{ title }}{{/listItems.one}}",
+         "extendedTemplate" : "{{#listItems.one}}{{> test-simple }}{{/listItems.one}}",
+         "key": "test-patternName",
+         "jsonFileData" : {},
+         "patternSpecificListJson" : {
+            "1": [
+               {
+                  "title": "One"
+               }
+            ],
+            "2": [
+                   {
+                      "title": "One"
+                   },
+                   {
+                      "title": "Two"
+                   },
+                ]
+          }
+      };
+
+      var patternlab = {
+        "listitems": {
+          "2": [
+             {
+                "title": "Foo"
+             },
+             {
+                "title": "Bar"
+             }
+          ]
+        },
+        "data": {
+          "link": {},
+          "partials": []
+        },
+        "config": {"debug": false},
+        "patterns": [
+          {
+           "template": "{{ title }}",
+           "extendedTemplate" : "{{ title }}",
+           "key": "test-simple",
+           "jsonFileData" : {}
+          }
+        ]
+      };
+
+      var list_item_hunter = new lih();
+
+      //act
+      list_item_hunter.process_list_item_partials(currentPattern, patternlab);
+
+      //assert
+      test.equals(currentPattern.extendedTemplate, "One" );
 
       test.done();
     }
