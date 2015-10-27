46f6b9326f82b27c1c5e2bf080daf292abf6dc18
Brad Frost <frostbd@gmail.com>
2015-10-27 12:44:11 -0400

    White space

--- packages/uikit-default/dist/js/patternlab-viewer.js
+++ packages/uikit-default/dist/js/patternlab-viewer.js
@@ -1177,7 +1177,7 @@ $('#sg-find .typeahead').blur(function() {
 
 // alert the iframe parent that the pattern has loaded assuming this view was loaded in an iframe
 if (self != top) {
-
+	
 	// handle the options that could be sent to the parent window
 	//   - all get path
 	//   - pattern & view all get a pattern partial, styleguide gets all
@@ -1185,15 +1185,15 @@ if (self != top) {
 	var path = window.location.toString();
 	var parts = path.split("?");
 	var options = { "event": "patternLab.pageLoad", "path": parts[0] };
-
+	
 	options.patternpartial = (patternData.patternPartial !== undefined) ? patternData.patternPartial : "all";
 	if (patternData.lineage !== "") {
 		options.lineage = patternData.lineage;
 	}
-
+	
 	var targetOrigin = (window.location.protocol == "file:") ? "*" : window.location.protocol+"//"+window.location.host;
 	parent.postMessage(options, targetOrigin);
-
+	
 	// find all links and add an onclick handler for replacing the iframe address so the history works
 	var aTags = document.getElementsByTagName('a');
 	for (var i = 0; i < aTags.length; i++) {
@@ -1211,7 +1211,7 @@ if (self != top) {
 			}
 		};
 	}
-
+	
 	// bind the keyboard shortcuts for various viewport resizings + pattern search
 	var keys = [ "s", "m", "l", "d", "h", "f" ];
 	for (var i = 0; i < keys.length; i++) {
@@ -1223,7 +1223,7 @@ if (self != top) {
 			};
 		}(keys[i],targetOrigin));
 	}
-
+	
 	// bind the keyboard shortcuts for mqs
 	var i = 0;
 	while (i < 10) {
@@ -1237,7 +1237,7 @@ if (self != top) {
 		}(i,targetOrigin));
 		i++;
 	}
-
+	
 }
 
 // if there are clicks on the iframe make sure the nav in the iframe parent closes
@@ -1245,46 +1245,45 @@ var body = document.getElementsByTagName('body');
 body[0].onclick = function() {
 	var targetOrigin = (window.location.protocol == "file:") ? "*" : window.location.protocol+"//"+window.location.host;
 	var obj = JSON.stringify({ "event": "patternLab.bodyClick", "bodyclick": "bodyclick" });
-	console.log('Body clicked: '+obj);
 	parent.postMessage(obj,targetOrigin);
 };
 
 // watch the iframe source so that it can be sent back to everyone else.
 function receiveIframeMessage(event) {
-
+	
 	// does the origin sending the message match the current host? if not dev/null the request
 	if ((window.location.protocol != "file:") && (event.origin !== window.location.protocol+"//"+window.location.host)) {
 		return;
 	}
-
+	
 	var path;
 	var data = (typeof event.data !== "string") ? event.data : JSON.parse(event.data);
-
+	
 	// see if it got a path to replace
 	if (data.event == "patternLab.updatePath") {
-
+		
 		if (patternData.patternPartial !== undefined) {
-
+			
 			// handle patterns and the view all page
 			var re = /(patterns|snapshots)\/(.*)$/;
 			path = window.location.protocol+"//"+window.location.host+window.location.pathname.replace(re,'')+data.path+'?'+Date.now();
 			window.location.replace(path);
-
+			
 		} else {
-
+			
 			// handle the style guide
 			path = window.location.protocol+"//"+window.location.host+window.location.pathname.replace("styleguide\/html\/styleguide.html","")+data.path+'?'+Date.now();
 			window.location.replace(path);
-
+			
 		}
-
+		
 	} else if (data.event == "patternLab.reload") {
-
+		
 		// reload the location if there was a message to do so
 		window.location.reload();
-
+		
 	}
-
+	
 }
 window.addEventListener("message", receiveIframeMessage, false);
 
