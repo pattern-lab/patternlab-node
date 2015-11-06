d1c2ce811ed7bdb3b1f506c5c7e3ddeea63c9121
Stefan Lehmann <stefan.lehmann@project-a.com>
2015-11-06 15:12:07 +0100

    chore (none): add autoprefixer module

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 // Special thanks to oscar-g (https://github.com/oscar-g) for starting this at https://github.com/oscar-g/patternlab-node/tree/dev-gulp
 
-var pkg = require('./package.json'),
-    gulp = require('gulp'),
-    eol = require('os').EOL,
-    del = require('del'),
-    strip_banner = require('gulp-strip-banner'),
-    header = require('gulp-header'),
-    nodeunit = require('gulp-nodeunit'),
-		sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create();
+var
+  pkg = require('./package.json'),
+  gulp = require('gulp'),
+  eol = require('os').EOL,
+  del = require('del'),
+  strip_banner = require('gulp-strip-banner'),
+  header = require('gulp-header'),
+  nodeunit = require('gulp-nodeunit'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  browserSync = require('browser-sync').create();
 
 require('gulp-load')(gulp);
-var banner = [ '/** ',
+var banner = ['/** ',
   ' * <%= pkg.name %> - v<%= pkg.version %> - <%= today %>',
   ' * ',
   ' * <%= pkg.author %>, and the web community.',
@@ -21,16 +23,16 @@ var banner = [ '/** ',
   ' * ', ' **/'].join(eol);
 
 //load patternlab-node tasks
-gulp.loadTasks(__dirname+'/builder/patternlab_gulp.js');
+gulp.loadTasks(__dirname + '/builder/patternlab_gulp.js');
 
 //clean patterns dir
-gulp.task('clean', function(cb){
+gulp.task('clean', function (cb) {
   del.sync(['./public/patterns/*'], {force: true});
   cb();
-})
+});
 
 //build the banner
-gulp.task('banner', function(){
+gulp.task('banner', function () {
   return gulp.src([
     './builder/patternlab.js',
     './builder/object_factory.js',
@@ -45,40 +47,41 @@ gulp.task('banner', function(){
     './builder/list_item_hunter.js'
   ])
     .pipe(strip_banner())
-    .pipe(header( banner, {
-      pkg : pkg,
-      today : new Date().getFullYear() }
+    .pipe(header(banner, {
+        pkg: pkg,
+        today: new Date().getFullYear()
+      }
     ))
     .pipe(gulp.dest('./builder'));
-})
+});
 
 //copy tasks
-gulp.task('cp:js', function(){
-  return gulp.src('**/*.js', {cwd:'./source/js'})
+gulp.task('cp:js', function () {
+  return gulp.src('**/*.js', {cwd: './source/js'})
     .pipe(gulp.dest('./public/js'))
 });
-gulp.task('cp:img', function(){
+gulp.task('cp:img', function () {
   return gulp.src(
-    [ '**/*.gif', '**/*.png', '**/*.jpg', '**/*.jpeg'  ],
-    {cwd:'./source/images'} )
+    ['**/*.gif', '**/*.png', '**/*.jpg', '**/*.jpeg'],
+    {cwd: './source/images'})
     .pipe(gulp.dest('./public/images'))
 });
-gulp.task('cp:font', function(){
-  return gulp.src('*', {cwd:'./source/fonts'})
+gulp.task('cp:font', function () {
+  return gulp.src('*', {cwd: './source/fonts'})
     .pipe(gulp.dest('./public/fonts'))
 });
-gulp.task('cp:data', function(){
-  return gulp.src('annotations.js', {cwd:'./source/_data'})
+gulp.task('cp:data', function () {
+  return gulp.src('annotations.js', {cwd: './source/_data'})
     .pipe(gulp.dest('./public/data'))
-})
-gulp.task('cp:css', function(){
+});
+gulp.task('cp:css', function () {
   return gulp.src('./source/css/style.css')
     .pipe(gulp.dest('./public/css'))
     .pipe(browserSync.stream());
-})
+});
 
 //server and watch tasks
-gulp.task('connect', ['lab'], function(){
+gulp.task('connect', ['lab'], function () {
   browserSync.init({
     server: {
       baseDir: './public/'
@@ -87,55 +90,62 @@ gulp.task('connect', ['lab'], function(){
   gulp.watch('./source/css/style.css', ['cp:css']);
 
   //suggested watches if you use scss
-   gulp.watch('./source/**/*.scss', ['sass:style']);
-   gulp.watch('./public/styleguide/*.scss', ['sass:styleguide']);
+  gulp.watch('./source/**/*.scss', ['sass:style']);
+  gulp.watch('./public/styleguide/*.scss', ['sass:styleguide']);
 
   gulp.watch([
-    './source/_patterns/**/*.mustache',
-    './source/_patterns/**/*.json',
-    './source/_data/*.json'	],
-     ['lab-pipe'], function(){
-       browserSync.reload();
-     });
+      './source/_patterns/**/*.mustache',
+      './source/_patterns/**/*.json',
+      './source/_data/*.json'],
+    ['lab-pipe'], function () {
+      browserSync.reload();
+    });
 
-})
+});
 
 //unit test
-gulp.task('nodeunit', function(){
+gulp.task('nodeunit', function () {
   return gulp.src('./test/**/*_tests.js')
     .pipe(nodeunit());
-})
+});
 
 //sass tasks, turn on if you want to use
- gulp.task('sass:style', function(){
- 	return gulp.src('./source/css/*.scss')
- 		.pipe(sass({
- 			outputStyle: 'expanded',
- 			precision: 8
- 		}))
- 		.pipe(gulp.dest('./public/css'))
-     .pipe(browserSync.stream());
- })
- gulp.task('sass:styleguide', function(){
- 	return gulp.src('./public/styleguide/css/*.scss')
-  		.pipe(sass({
-  			outputStyle: 'expanded',
-  			precision: 8
-  		}))
-  		.pipe(gulp.dest('./public/styleguide/css'))
-     .pipe(browserSync.stream());
- })
+gulp.task('sass:style', function () {
+  return gulp.src('./source/css/*.scss')
+    .pipe(sass({
+      outputStyle: 'expanded',
+      precision: 8
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream());
+});
 
-gulp.task('lab-pipe', ['lab'], function(cb){
+gulp.task('sass:styleguide', function () {
+  return gulp.src('./public/styleguide/css/*.scss')
+    .pipe(sass({
+      outputStyle: 'expanded',
+      precision: 8
+    }))
+    .pipe(gulp.dest('./public/styleguide/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('lab-pipe', ['lab'], function (cb) {
   cb();
   browserSync.reload();
-})
+});
 
 gulp.task('default', ['lab']);
 
 gulp.task('assets', ['cp:js', 'cp:img', 'cp:font', 'cp:data', 'sass:style', 'sass:styleguide']);
 gulp.task('prelab', ['clean', 'banner', 'assets']);
-gulp.task('lab', ['prelab', 'patternlab'], function(cb){cb();});
+gulp.task('lab', ['prelab', 'patternlab'], function (cb) {
+  cb();
+});
 gulp.task('patterns', ['patternlab:only_patterns']);
 gulp.task('serve', ['lab', 'connect']);
 gulp.task('travis', ['lab', 'nodeunit']);
