617f4fed90ff2ff608e8a39117282a6a325af83c
Marimar Hollenbach <marimar.hollenbach@project-a.com>
2015-11-13 18:14:14 +0100

    Refactor (HSK-386) Add patternlab class to avoid conflicts

--- source/_patterns/01-molecules/06-components/01-accordion.js
+++ source/_patterns/01-molecules/06-components/01-accordion.js
@@ -2,6 +2,7 @@ module.exports = (function() {
     'use strict';
 
     var $ = require('jquery');
-    $('.accordion .something').click();
-
+    $('[class*=accordion__handle]').click(function(){
+        $(this).toggleClass($(this).hasClass('accordion__handle') ? 'accordion__handle--expanded' : 'mobile-accordion__handle--expanded');
+    });
 })();
