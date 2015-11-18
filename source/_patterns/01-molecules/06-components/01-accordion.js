43dbc46c47f9e8c3f8247d8c17826aaebecef8b7
Marimar Hollenbach <marimar.hollenbach@project-a.com>
2015-11-18 16:01:14 +0100

    Styling (HSK-386) Add missing styling and patternlab class to encapsulate new CSS

--- source/_patterns/01-molecules/06-components/01-accordion.js
+++ source/_patterns/01-molecules/06-components/01-accordion.js
@@ -1,8 +1,6 @@
-module.exports = (function() {
-    'use strict';
-
-    var $ = require('jquery');
-    $('[class*=accordion__handle]').click(function(){
+var $ = require('jquery');
+module.exports = $(function() {
+    $('body').on('click', '[class*=accordion__handle]', function(){
         $(this).toggleClass($(this).hasClass('accordion__handle') ? 'accordion__handle--expanded' : 'mobile-accordion__handle--expanded');
     });
-})();
+});
