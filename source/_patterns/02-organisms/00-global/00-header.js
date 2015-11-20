e4e4ec4ff54b9061116d7ff19fb72b516c009578
Marimar Hollenbach <marimar.hollenbach@project-a.com>
2015-11-20 17:36:45 +0100

    Styling & Feature (HSK-386) functioning sticky header on scroll

--- source/_patterns/02-organisms/00-global/00-header.js
+++ source/_patterns/02-organisms/00-global/00-header.js
@@ -1,7 +1,15 @@
 var $ = require('jquery');
 module.exports = $(function() {
     $('body').on('click', '.navigation--primary__hamburger', function(e) {
-        e.preventDefault();
-        $(this).toggleClass('navigation--active');
+        e.stopPropagation();
+        $('.basicLayout').toggleClass('basicLayout--showOffCanvasA');
+    }).on('click', '.basicLayout__canvas', function() {
+        $('.basicLayout').removeClass('basicLayout--showOffCanvasA');
+    }).on('click', '.header__navigation', function(e) {
+        e.stopPropagation();
+    });
+    $(document).scroll(function() {
+        $('header.header')[$(window).scrollTop() ? 'addClass' : 'removeClass']('header--sticky');
+        $('.pageContent')[$(window).scrollTop() ? 'addClass' : 'removeClass']('pageContent--stickyHeader');
     });
 });
