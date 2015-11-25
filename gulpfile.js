d19d38e05baef4f741f429fd0a0ae7c319141303
Marimar Hollenbach <marimar.hollenbach@project-a.com>
2015-11-25 15:16:42 +0100

    Feature & Styling (HSK-369) adds icons

--- gulpfile.js
+++ gulpfile.js
@@ -167,9 +167,17 @@ gulp.task('sass:styleguide', function () {
 });
 
 gulp.task('iconfont', function() {
-  var iconfont = require('gulp-iconfont');
+  var iconfont = require('gulp-iconfont'),
+      iconfontCss = require('gulp-iconfont-css'),
+      fs = require('fs');
 
-  return gulp.src('./source/fonts/icon-source/**/*.svg')
+  return gulp.src('./source/_patterns/00-atoms/03-images/_icons/**/*.svg')
+      .pipe(iconfontCss({
+          fontName: 'horizn-icons',
+          path: 'scss',
+          targetPath: '../../source/_patterns/00-atoms/03-images/05-icons.scss',
+          fontPath: '../fonts/'
+      }))
       .pipe(iconfont({
         fontName: 'horizn-icons',
         appendUnicode: true,
@@ -177,6 +185,12 @@ gulp.task('iconfont', function() {
         formats: ['ttf', 'woff', 'woff2'],
         timestamp: new Date().getTime()
       }))
+      .on('glyphs', function(glyphs) {
+          var html = '<!-- This file is generated, don\'t change! -->\n\n<div class="icons patternlab">\n' + glyphs.map(function(glyph) {
+              return '\t<span class="icon-' + glyph.name + '"></span>\n\t<div class="icons__description">icon-' + glyph.name + '</div>';
+          }).join('\n\n') + '\n</div>';
+          fs.writeFile('./source/_patterns/00-atoms/03-images/05-icons.mustache', html);
+      })
       .pipe(gulp.dest('./public/fonts'))
       ;
 });
