ab359fba5a774f648e16ffef0a7530d605eee67c
Brian Muenzenmeyer <bmuenzenmeyer@sbcglobal.net>
2015-11-26 15:58:41 -0600

    Merge pull request #191 from geoffp/pattern-engines
    PatternEngines: support multiple template engines, v2

--- packages/patternengine-node-handlebars/test/lineage_hunter_tests.js
+++ packages/patternengine-node-handlebars/test/lineage_hunter_tests.js
@@ -2,29 +2,34 @@
 	"use strict";
 
 	var lh = require('../builder/lineage_hunter');
+  var of = require('../builder/object_factory');
+  var extend = require('util')._extend;
+
+  // fake pattern creators
+  function createFakeEmptyErrorPattern() {
+    return new of.oPattern(
+      '/home/fakeuser/pl/source/_patterns/01-molecules/01-toast/00-error.mustache', // abspath
+      '01-molecules\\01-toast', // subdir
+      '00-error.mustache', // filename,
+      null // data
+    );
+  }
 
 	exports['lineage hunter '] = {
 		'test lineage hunter finds lineage' : function(test){
 
 			//setup current pattern from what we would have during execution
-			var currentPattern = {
-				"name": "02-organisms-00-global-00-header",
-				"subdir": "02-organisms\\00-global",
-				"filename": "00-header.mustache",
-				"data": null,
+      var currentPattern = new of.oPattern(
+        '/home/fakeuser/pl/source/_patterns/02-organisms/00-global/00-header.mustache', // abspath
+        '02-organisms\\00-global', // subdir
+        '00-header.mustache', // filename,
+        null // data
+      );
+			extend(currentPattern, {
 				"template": "<!-- Begin .header -->\r\n<header class=\"header cf\" role=\"banner\">\r\n\t{{> atoms-logo }}\r\n\t<a href=\"#\" class=\"nav-toggle nav-toggle-search icon-search\"><span class=\"is-vishidden\">Search</span></a>\r\n\t<a href=\"#\" class=\"nav-toggle nav-toggle-menu icon-menu\"><span class=\"is-vishidden\">Menu</span></a>\r\n\t{{> molecules-primary-nav }}\r\n\t{{> molecules-search }}\r\n</header>\r\n<!-- End .header -->\r\n",
-				"patternPartial": "<!-- Begin .header -->\r\n<header class=\"header cf\" role=\"banner\">\r\n<a href=\"/\"><img src=\"../../images/logo.png\" class=\"logo\" alt=\"Logo Alt Text\" /></a>\t<a href=\"#\" class=\"nav-toggle nav-toggle-search icon-search\"><span class=\"is-vishidden\">Search</span></a>\r\n\t<a href=\"#\" class=\"nav-toggle nav-toggle-menu icon-menu\"><span class=\"is-vishidden\">Menu</span></a>\r\n<nav id=\"nav\" class=\"nav\">\r\n\t<ul>\r\n\t\t<li><a href=\"#\">Home</a></li>\r\n\t\t<li><a href=\"#\">About</a></li>\r\n\t\t<li><a href=\"#\">Blog</a></li>\r\n\t\t<li><a href=\"#\">Contact</a></li>\r\n\t</ul>\r\n</nav><!--end .nav-->\r\n<form action=\"#\" method=\"post\" class=\"inline-form search-form\">           \r\n    <fieldset>\r\n\t    <legend class=\"is-vishidden\">Search</legend>\r\n\t    <label for=\"search-field\" class=\"is-vishidden\">Search</label>\r\n\t    <input type=\"search\" placeholder=\"Search\" id=\"search-field\" class=\"search-field\" />\r\n\t    <button class=\"search-submit\">\r\n\t    \t<span class=\"icon-search\" aria-hidden=\"true\"></span>\r\n\t    \t<span class=\"is-vishidden\">Search</span>\r\n\t    </button>\r\n    </fieldset>\r\n</form></header>\r\n<!-- End .header -->\r\n",
-				"patternName": "header",
-				"patternLink": "02-organisms-00-global-00-header/02-organisms-00-global-00-header.html",
-				"patternGroup": "organisms",
-				"patternSubGroup": "organisms\\00-global",
-				"flatPatternPath": "02-organisms\\00-global",
-				"patternState": "",
-				"lineage": [],
-				"lineageIndex": [],
-				"lineageR": [],
-				"lineageRIndex": []
-			};
+				"patternPartial": "<!-- Begin .header -->\r\n<header class=\"header cf\" role=\"banner\">\r\n<a href=\"/\"><img src=\"../../images/logo.png\" class=\"logo\" alt=\"Logo Alt Text\" /></a>\t<a href=\"#\" class=\"nav-toggle nav-toggle-search icon-search\"><span class=\"is-vishidden\">Search</span></a>\r\n\t<a href=\"#\" class=\"nav-toggle nav-toggle-menu icon-menu\"><span class=\"is-vishidden\">Menu</span></a>\r\n<nav id=\"nav\" class=\"nav\">\r\n\t<ul>\r\n\t\t<li><a href=\"#\">Home</a></li>\r\n\t\t<li><a href=\"#\">About</a></li>\r\n\t\t<li><a href=\"#\">Blog</a></li>\r\n\t\t<li><a href=\"#\">Contact</a></li>\r\n\t</ul>\r\n</nav><!--end .nav-->\r\n<form action=\"#\" method=\"post\" class=\"inline-form search-form\">           \r\n    <fieldset>\r\n\t    <legend class=\"is-vishidden\">Search</legend>\r\n\t    <label for=\"search-field\" class=\"is-vishidden\">Search</label>\r\n\t    <input type=\"search\" placeholder=\"Search\" id=\"search-field\" class=\"search-field\" />\r\n\t    <button class=\"search-submit\">\r\n\t    \t<span class=\"icon-search\" aria-hidden=\"true\"></span>\r\n\t    \t<span class=\"is-vishidden\">Search</span>\r\n\t    </button>\r\n    </fieldset>\r\n</form></header>\r\n<!-- End .header -->\r\n"
+			});
+
 			var patternlab = {
 				patterns: [
 				{
@@ -97,24 +102,12 @@
 
 		'test lineage hunter finds lineage with spaced pattern parameters' : function(test){
 			//setup current pattern from what we would have during execution
-			var currentPattern = {
-				"name": "01-molecules-01-toast-00-error",
-				"subdir": "01-molecules\\01-toast",
-				"filename": "00-error.mustache",
-				"data": null,
+      var currentPattern = createFakeEmptyErrorPattern();
+      extend(currentPattern, {
 				"template": "{{> atoms-error(message: 'That\'s no moon...') }}",
-				"patternPartial": "{{> atoms-error(message: 'That\'s no moon...') }}",
-				"patternName": "error",
-				"patternLink": "01-molecules-01-toast-00-error/01-molecules-01-toast-00-error.html",
-				"patternGroup": "molecules",
-				"patternSubGroup": "molecules\\01-toast",
-				"flatPatternPath": "01-molecules\\01-toast",
-				"patternState": "",
-				"lineage": [],
-				"lineageIndex": [],
-				"lineageR": [],
-				"lineageRIndex": []
-			};
+				"patternPartial": "{{> atoms-error(message: 'That\'s no moon...') }}"
+      });
+
 			var patternlab = {
 				patterns: [
 					{
@@ -149,24 +142,12 @@
 
 		'test lineage hunter finds lineage with unspaced pattern parameters' : function(test){
 			//setup current pattern from what we would have during execution
-			var currentPattern = {
-				"name": "01-molecules-01-toast-00-error",
-				"subdir": "01-molecules\\01-toast",
-				"filename": "00-error.mustache",
-				"data": null,
+      var currentPattern = createFakeEmptyErrorPattern();
+      extend(currentPattern, {
 				"template": "{{>atoms-error(message: 'That\'s no moon...')}}",
 				"patternPartial": "{{>atoms-error(message: 'That\'s no moon...')}}",
-				"patternName": "error",
-				"patternLink": "01-molecules-01-toast-00-error/01-molecules-01-toast-00-error.html",
-				"patternGroup": "molecules",
-				"patternSubGroup": "molecules\\01-toast",
-				"flatPatternPath": "01-molecules\\01-toast",
-				"patternState": "",
-				"lineage": [],
-				"lineageIndex": [],
-				"lineageR": [],
-				"lineageRIndex": []
-			};
+      });
+
 			var patternlab = {
 				patterns: [
 					{
@@ -203,24 +184,11 @@
 
 		'test lineage hunter does not apply lineage twice' : function(test){
 			//setup current pattern from what we would have during execution
-			var currentPattern = {
-				"name": "01-molecules-01-toast-00-error",
-				"subdir": "01-molecules\\01-toast",
-				"filename": "00-error.mustache",
-				"data": null,
+      var currentPattern = createFakeEmptyErrorPattern();
+      extend(currentPattern, {
 				"template": "{{>atoms-error(message: 'That\'s no moon...')}}",
-				"patternPartial": "{{>atoms-error(message: 'That\'s no moon...')}}",
-				"patternName": "error",
-				"patternLink": "01-molecules-01-toast-00-error/01-molecules-01-toast-00-error.html",
-				"patternGroup": "molecules",
-				"patternSubGroup": "molecules\\01-toast",
-				"flatPatternPath": "01-molecules\\01-toast",
-				"patternState": "",
-				"lineage": [],
-				"lineageIndex": [],
-				"lineageR": [],
-				"lineageRIndex": []
-			};
+				"patternPartial": "{{>atoms-error(message: 'That\'s no moon...')}}"
+			});
 			var patternlab = {
 				patterns: [
 					{
@@ -254,7 +222,7 @@
 			test.equals(JSON.parse(patternlab.patterns[0].lineageR).lineagePattern, 'molecules-error');
 
 			test.done();
-		},
+		}
 
 
 	};
