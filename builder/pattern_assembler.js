3dfe791c022bfdce7419536bf1e1425a24fb3aa1
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2015-11-26 15:11:39 -0600

    resolve issue with not hiding underscored patterns. fixes #165

--- builder/pattern_assembler.js
+++ builder/pattern_assembler.js
@@ -1,10 +1,10 @@
-/*
- * patternlab-node - v0.15.1 - 2015
- *
+/* 
+ * patternlab-node - v0.15.2 - 2015 
+ * 
  * Brian Muenzenmeyer, and the web community.
- * Licensed under the MIT license.
- *
- * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice.
+ * Licensed under the MIT license. 
+ * 
+ * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice. 
  *
  */
 
@@ -94,8 +94,8 @@
       var filename = path.basename(file);
       var ext = path.extname(filename);
 
-      //ignore dotfiles and non-variant .json files
-      if(filename.charAt(0) === '.' || (ext === '.json' && filename.indexOf('~') === -1)){
+      //ignore dotfiles, underscored files, and non-variant .json files
+      if(filename.charAt(0) === '.' || filename.charAt(0) === '_' || (ext === '.json' && filename.indexOf('~') === -1)){
         return;
       }
 
--- builder/patternlab_grunt.js
+++ builder/patternlab_grunt.js
@@ -1,5 +1,5 @@
 /* 
- * patternlab-node - v0.15.1 - 2015 
+ * patternlab-node - v0.15.2 - 2015 
  * 
  * Brian Muenzenmeyer, and the web community.
  * Licensed under the MIT license. 
