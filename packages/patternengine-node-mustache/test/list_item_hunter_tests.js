570bfefba6974c53f6509206282784c9a5ab9aea
Brian Muenzenmeyer <bmuenzenmeyer@sbcglobal.net>
2015-11-26 15:58:41 -0600

    Merge pull request #191 from geoffp/pattern-engines
    PatternEngines: support multiple template engines, v2

--- packages/patternengine-node-mustache/test/list_item_hunter_tests.js
+++ packages/patternengine-node-mustache/test/list_item_hunter_tests.js
@@ -2,17 +2,31 @@
   "use strict";
 
   var lih = require('../builder/list_item_hunter');
+  var of = require('../builder/object_factory');
+  var extend = require('util')._extend;
+
+  // fake pattern creators
+  function createFakeListPattern(customProps) {
+    var inputs = {
+      abspath: '/home/fakeuser/pl/source/_patterns/01-molecules/01-lists/00-list.mustache',
+      subdir: '01-molecules\\01-lists',
+      filename: '00-list.mustache',
+      data: {}
+    };
+    var pattern = new of.oPattern(inputs.abspath, inputs.subdir, inputs.filename, inputs.data);
+
+    return extend(pattern, customProps);
+  }
 
   exports['list_item_hunter'] = {
     'process_list_item_partials finds and outputs basic repeating blocks' : function(test){
       //arrange
       //setup current pattern from what we would have during execution
-      var currentPattern = {
-         "template": "{{#listItems.two}}{{ title }}{{/listItems.two}}",
-         "extendedTemplate" : "{{#listItems.two}}{{ title }}{{/listItems.two}}",
-         "key": "test-patternName",
-         "jsonFileData" : {}
-      };
+      var currentPattern = createFakeListPattern({
+        "template": "{{#listItems.two}}{{ title }}{{/listItems.two}}",
+        "extendedTemplate": "{{#listItems.two}}{{ title }}{{/listItems.two}}",
+        "key": "test-patternName"
+      });
 
       var patternlab = {
         "listitems": {
@@ -51,12 +65,11 @@
     'process_list_item_partials listitems with lowercase name' : function(test){
       //arrange
       //setup current pattern from what we would have during execution
-      var currentPattern = {
-         "template": "{{#listitems.two}}{{ title }}{{/listitems.two}}",
-         "extendedTemplate" : "{{#listitems.two}}{{ title }}{{/listitems.two}}",
-         "key": "test-patternName",
-         "jsonFileData" : {}
-      };
+      var currentPattern = createFakeListPattern({
+        "template": "{{#listitems.two}}{{ title }}{{/listitems.two}}",
+        "extendedTemplate" : "{{#listitems.two}}{{ title }}{{/listitems.two}}",
+        "key": "test-patternName"
+      });
 
       var patternlab = {
         "listitems": {
@@ -95,12 +108,11 @@
     'process_list_item_partials finds partials and outputs repeated renders' : function(test){
       //arrange
       //setup current pattern from what we would have during execution
-      var currentPattern = {
-         "template": "{{#listItems.two}}{{ title }}{{/listItems.two}}",
-         "extendedTemplate" : "{{#listItems.two}}{{> test-simple }}{{/listItems.two}}",
-         "key": "test-patternName",
-         "jsonFileData" : {}
-      };
+      var currentPattern = createFakeListPattern({
+        "template": "{{#listItems.two}}{{ title }}{{/listItems.two}}",
+        "extendedTemplate" : "{{#listItems.two}}{{> test-simple }}{{/listItems.two}}",
+        "key": "test-patternName"
+      });
 
       var patternlab = {
         "listitems": {
@@ -145,19 +157,17 @@
     },
 
     'process_list_item_partials finds verbose partials and outputs repeated renders' : function(test){
-      var pattern1 = {
+      var pattern1 = createFakeListPattern({
         "template": "{{#listItems.one}}{{> 00-test/00-foo }}{{/listItems.one}}",
         "extendedTemplate" : "{{#listItems.one}}{{> 00-test/00-foo }}{{/listItems.one}}",
-        "key": "test-patternName1",
-        "jsonFileData" : {}
-      };
+        "key": "test-patternName1"
+      });
 
-      var pattern2 = {
+      var pattern2 = createFakeListPattern({
         "template": "{{#listItems.two}}{{> 00-test/01-bar.mustache }}{{/listItems.two}}",
         "extendedTemplate" : "{{#listItems.two}}{{> 00-test/01-bar.mustache }}{{/listItems.two}}",
-        "key": "test-patternName2",
-        "jsonFileData" : {}
-      };
+        "key": "test-patternName2"
+      });
 
       var patternlab = {
         "listitems": {
@@ -181,20 +191,16 @@
         },
         "config": {"debug": false},
         "patterns": [
-          {
-           "template": "{{ title }}",
-           "extendedTemplate" : "{{ title }}",
-           "subdir": "00-test",
-           "fileName": "00-foo",
-           "jsonFileData" : {}
-          },
-          {
-           "template": "{{ title }}",
-           "extendedTemplate" : "{{ title }}",
-           "subdir": "00-test",
-           "fileName": "01-bar",
-           "jsonFileData" : {}
-          }
+          of.oPattern.create(null, "00-test", "00-foo", null, {
+            "template": "{{ title }}",
+            "extendedTemplate": "{{ title }}",
+            "jsonFileData": {}
+          }),
+          of.oPattern.create(null, "00-test", "00-bar", null, {
+            "template": "{{ title }}",
+            "extendedTemplate": "{{ title }}",
+            "jsonFileData": {}
+          })
         ]
       };
 
