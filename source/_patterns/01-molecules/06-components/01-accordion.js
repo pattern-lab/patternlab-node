b8d0ea94130150a3172eb593bbf21fb48c068164
Stefan Lehmann <stefan.lehmann@project-a.com>
2015-11-30 12:03:51 +0100

    chore (none) remove files that are not covering the test case

--- source/_patterns/01-molecules/06-components/01-accordion.js
+++
@@ -1,6 +0,0 @@
-var $ = require('jquery');
-module.exports = $(function() {
-    $('body').on('click', '[class*=js-accordion__handle]', function(){
-        $(this).toggleClass($(this).hasClass('accordion__handle') ? 'accordion__handle--expanded' : 'mobile-accordion__handle--expanded');
-    });
-});
