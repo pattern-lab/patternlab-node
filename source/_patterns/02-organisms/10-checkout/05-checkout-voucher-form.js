12496dcddaaada3a3a6d2d7a8cc99468b7ae34fa
Marimar Hollenbach <marimar.hollenbach@project-a.com>
2015-12-07 11:08:32 +0100

    Feature (HSK-402) create voucher code toggle for checkout

--- source/_patterns/02-organisms/10-checkout/05-checkout-voucher-form.js
+++ source/_patterns/02-organisms/10-checkout/05-checkout-voucher-form.js
@@ -2,5 +2,6 @@ var $ = require('jquery');
 module.exports = $(function() {
     $('body').on('click', '[class*=checkout__voucherField--label]', function(){
         $(this).toggleClass($(this).hasClass('checkout__voucherField--label') ? 'checkout__voucherField--label--expanded' : 'checkout__voucherField--label--expanded');
+        $('.field').toggleClass('field--expanded');
     });
 });
