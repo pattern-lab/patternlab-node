b188dc06315bbd7778229b440f8b09e9484d6ede
Thomas Bouron <tbouron@gmail.com>
2016-02-09 22:00:44 +0000

    Downstream projects can now require grunt and gulp tasks, no need to copy them anymore.

--- builder/patternlab_grunt.js
+++ builder/patternlab_grunt.js
@@ -1,16 +1,137 @@
-/* 
- * patternlab-node - v1.1.0 - 2016 
- * 
+/*
+ * patternlab-node - v1.1.0 - 2016
+ *
  * Brian Muenzenmeyer, and the web community.
- * Licensed under the MIT license. 
- * 
- * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice. 
+ * Licensed under the MIT license.
+ *
+ * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice.
  *
  */
 
-var patternlab_engine = require('./patternlab.js');
+// IMPORTANT: Remember that `./` matches the root of the grunt file that requires this one!
+// However, this does not apply to the `require('...')`
+module.exports = function(grunt, config) {
+	var patternlab_engine = require('./patternlab.js'),
+		path = require('path');
 
-module.exports = function(grunt) {
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				stripBanners: true,
+				banner: '/* \n * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy") %> \n * \n * <%= pkg.author %>, and the web community.\n * Licensed under the <%= pkg.license %> license. \n * \n * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice. \n *\n */\n\n',
+			},
+			patternlab: {
+				src: './builder/patternlab.js',
+				dest: './builder/patternlab.js'
+			},
+			object_factory: {
+				src: './builder/object_factory.js',
+				dest: './builder/object_factory.js'
+			},
+			lineage: {
+				src: './builder/lineage_hunter.js',
+				dest: './builder/lineage_hunter.js'
+			},
+			media_hunter: {
+				src: './builder/media_hunter.js',
+				dest: './builder/media_hunter.js'
+			},
+			patternlab_grunt: {
+				src: './builder/patternlab_grunt.js',
+				dest: './builder/patternlab_grunt.js'
+			},
+			patternlab_gulp: {
+				src: './builder/patternlab_gulp.js',
+				dest: './builder/patternlab_gulp.js'
+			},
+			parameter_hunter: {
+				src: './builder/parameter_hunter.js',
+				dest: './builder/parameter_hunter.js'
+			},
+			pattern_exporter: {
+				src: './builder/pattern_exporter.js',
+				dest: './builder/pattern_exporter.js'
+			},
+			pattern_assembler: {
+				src: './builder/pattern_assembler.js',
+				dest: './builder/pattern_assembler.js'
+			},
+			pseudopattern_hunter: {
+				src: './builder/pseudopattern_hunter.js',
+				dest: './builder/pseudopattern_hunter.js'
+			},
+			list_item_hunter: {
+				src: './builder/list_item_hunter.js',
+				dest: './builder/list_item_hunter.js'
+			},
+			style_modifier_hunter: {
+				src: './builder/style_modifier_hunter.js',
+				dest: './builder/style_modifier_hunter.js'
+			}
+		},
+		copy: {
+			main: {
+				files: [
+					{ expand: true, cwd: path.resolve(config.paths.source.js), src: '*.js', dest: path.resolve(config.paths.public.js) },
+					{ expand: true, cwd: path.resolve(config.paths.source.css), src: '*.css', dest: path.resolve(config.paths.public.css) },
+					{ expand: true, cwd: path.resolve(config.paths.source.images), src: ['**/*.png', '**/*.jpg', '**/*.gif', '**/*.jpeg'], dest: path.resolve(config.paths.public.images) },
+					{ expand: true, cwd: path.resolve(config.paths.source.fonts), src: '*', dest: path.resolve(config.paths.public.fonts) },
+					{ expand: true, cwd: path.resolve(config.paths.source.data), src: 'annotations.js', dest: path.resolve(config.paths.public.data) }
+				]
+			},
+			styleguide: {
+				files: [
+					{ expand: true, cwd: path.resolve(config.paths.source.styleguide), src: ['*.*', '**/*.*'], dest: path.resolve(config.paths.public.styleguide) }
+				]
+			}
+		},
+		watch: {
+			all: {
+				files: [
+					path.resolve(config.paths.source.css + '**/*.css'),
+					path.resolve(config.paths.source.styleguide + 'css/*.css'),
+					path.resolve(config.paths.source.patterns + '**/*.mustache'),
+					path.resolve(config.paths.source.patterns + '**/*.json'),
+					path.resolve(config.paths.source.fonts + '/*'),
+					path.resolve(config.paths.source.images + '/*'),
+					path.resolve(config.paths.source.data + '*.json')
+				],
+				tasks: ['default', 'bsReload:css']
+			}
+		},
+		nodeunit: {
+			all: ['test/*_tests.js']
+		},
+		browserSync: {
+			dev: {
+				options: {
+					server:  path.resolve(config.paths.public.root),
+					watchTask: true,
+					watchOptions: {
+						ignoreInitial: true,
+						ignored: '*.html'
+					},
+					plugins: [
+						{
+							module: 'bs-html-injector',
+							options: {
+								files: [path.resolve(config.paths.public.root + '/index.html'), path.resolve(config.paths.public.styleguide + '/styleguide.html')]
+							}
+						}
+					]
+				}
+			}
+		},
+		bsReload: {
+			css: path.resolve(config.paths.public.root + '**/*.css')
+		}
+	});
+
+	// load all grunt tasks
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
 	grunt.registerTask('patternlab', 'create design systems with atomic design', function(arg) {
 
 		var patternlab = patternlab_engine();
@@ -36,5 +157,14 @@ module.exports = function(grunt) {
 		}
 
 	});
+
+	grunt.registerTask('default', ['patternlab', 'copy:main', 'copy:styleguide']);
+
+	//travis CI task
+	grunt.registerTask('travis', ['nodeunit', 'patternlab']);
+
+	grunt.registerTask('serve', ['patternlab', 'copy:main', 'copy:styleguide', 'browserSync', 'watch:all']);
+
+	grunt.registerTask('build', ['nodeunit', 'concat']);
 
 };
--- builder/patternlab_gulp.js
+++ builder/patternlab_gulp.js
@@ -1,17 +1,27 @@
-/* 
- * patternlab-node - v1.1.0 - 2016 
- * 
+/*
+ * patternlab-node - v1.1.0 - 2016
+ *
  * Brian Muenzenmeyer, and the web community.
- * Licensed under the MIT license. 
- * 
- * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice. 
+ * Licensed under the MIT license.
+ *
+ * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice.
  *
  */
 
-var patternlab_engine = require('./patternlab.js');
+ // IMPORTANT: Remember that `./` matches the root of the gulp file that requires this one!
+ // However, this does not apply to the `require('...')`
+module.exports = function(gulp, config) {
+  var patternlab_engine = require('./patternlab.js'),
+      pkg = require('./../package.json'),
+      path = require('path'),
+      eol = require('os').EOL,
+      del = require('del'),
+      strip_banner = require('gulp-strip-banner'),
+      header = require('gulp-header'),
+      nodeunit = require('gulp-nodeunit'),
+      browserSync = require('browser-sync').create();
 
-module.exports = function(gulp) {
-
+  // Patternlab tasks
   gulp.task('patternlab', ['clean'], function(cb){
     var patternlab = patternlab_engine();
     patternlab.build(false);
@@ -32,5 +42,139 @@ module.exports = function(gulp) {
     var patternlab = patternlab_engine();
     patternlab.help();
   });
+
+  var banner = [ '/** ',
+    ' * <%= pkg.name %> - v<%= pkg.version %> - <%= today %>',
+    ' * ',
+    ' * <%= pkg.author %>, and the web community.',
+    ' * Licensed under the <%= pkg.license %> license.',
+    ' * ',
+    ' * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice.',
+    ' * ', ' **/'].join(eol);
+
+  //clean patterns dir
+  gulp.task('clean', function(cb){
+    del.sync([path.resolve(config.paths.public.patterns, '*')], {force: true});
+    cb();
+  });
+
+  //build the banner
+  gulp.task('banner', function(){
+    return gulp.src([
+      './builder/patternlab.js',
+      './builder/object_factory.js',
+      './builder/lineage_hunter.js',
+      './builder/media_hunter.js',
+      './builder/patternlab_grunt.js',
+      './builder/patternlab_gulp.js',
+      './builder/parameter_hunter.js',
+      './builder/pattern_exporter.js',
+      './builder/pattern_assembler.js',
+      './builder/pseudopattern_hunter.js',
+      './builder/list_item_hunter.js',
+      './builder/style_modifier_hunter.js'
+    ])
+      .pipe(strip_banner())
+      .pipe(header( banner, {
+        pkg : pkg,
+        today : new Date().getFullYear() }
+      ))
+      .pipe(gulp.dest('./builder'));
+  });
+
+
+  // COPY TASKS
+
+  // JS copy
+  gulp.task('cp:js', function(){
+    return gulp.src('**/*.js', {cwd: path.resolve(config.paths.source.js)} )
+      .pipe(gulp.dest(path.resolve(config.paths.public.js)));
+  });
+
+  // Images copy
+  gulp.task('cp:img', function(){
+    return gulp.src(
+      [ '**/*.gif', '**/*.png', '**/*.jpg', '**/*.jpeg'  ],
+      {cwd: path.resolve(config.paths.source.images)} )
+      .pipe(gulp.dest(path.resolve(config.paths.public.images)));
+  });
+
+  // Fonts copy
+  gulp.task('cp:font', function(){
+    return gulp.src('*', {cwd: path.resolve(config.paths.source.fonts)})
+      .pipe(gulp.dest(path.resolve(config.paths.public.images)));
+  });
+
+  // Data copy
+  gulp.task('cp:data', function(){
+    return gulp.src('annotations.js', {cwd: path.resolve(config.paths.source.data)})
+      .pipe(gulp.dest(path.resolve(config.paths.public.data)));
+  });
+
+  // CSS Copy
+  gulp.task('cp:css', function(){
+    return gulp.src(path.resolve(config.paths.source.css, 'style.css'))
+      .pipe(gulp.dest(path.resolve(config.paths.public.css)))
+      .pipe(browserSync.stream());
+  });
+
+  // Styleguide Copy
+  gulp.task('cp:styleguide', function(){
+    return gulp.src(
+        [ '**/*'],
+        {cwd: path.resolve(config.paths.source.styleguide)} )
+        .pipe(gulp.dest(path.resolve(config.paths.public.styleguide)))
+        .pipe(browserSync.stream());;
+  });
+
+  //server and watch tasks
+  gulp.task('connect', ['lab'], function(){
+    browserSync.init({
+      server: {
+        baseDir: path.resolve(config.paths.public.root)
+      }
+    });
+    gulp.watch(path.resolve(config.paths.source.css, '**/*.css'), ['cp:css']);
+
+    gulp.watch(path.resolve(config.paths.source.styleguide, '**/*.*'), ['cp:styleguide']);
+
+    gulp.watch(
+      [
+        path.resolve(config.paths.source.patterns, '**/*.mustache'),
+        path.resolve(config.paths.source.patterns, '**/*.json'),
+        path.resolve(config.paths.source.data, '*.json'),
+        path.resolve(config.paths.source.fonts + '/*'),
+        path.resolve(config.paths.source.images + '/*'),
+        path.resolve(config.paths.source.data + '*.json'),
+      ],
+      ['lab-pipe'],
+      function () { browserSync.reload(); }
+    );
+
+  });
+
+  //unit test
+  gulp.task('nodeunit', function(){
+    return gulp.src('./test/**/*_tests.js')
+      .pipe(nodeunit());
+  });
+
+
+  gulp.task('lab-pipe', ['lab'], function(cb){
+    cb();
+    browserSync.reload();
+  });
+
+  gulp.task('default', ['lab']);
+
+  gulp.task('assets', ['cp:js', 'cp:img', 'cp:font', 'cp:data', 'cp:css', 'cp:styleguide' ]);
+  gulp.task('prelab', ['clean', 'assets']);
+  gulp.task('lab', ['prelab', 'patternlab'], function(cb){cb();});
+  gulp.task('patterns', ['patternlab:only_patterns']);
+  gulp.task('serve', ['lab', 'connect']);
+  gulp.task('travis', ['lab', 'nodeunit']);
+
+  gulp.task('version', ['patternlab:version']);
+  gulp.task('help', ['patternlab:help']);
 
 };
