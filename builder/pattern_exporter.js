d2ff6ea4c1fc1d93c9db5a220695bce8c7957a7c
New Geoff <newgeoff@geoff-diningroom.pursell.net>
2016-02-27 15:40:40 -0600

    most of the rest of the builder JS, reformatted

--- builder/pattern_exporter.js
+++ builder/pattern_exporter.js
@@ -8,38 +8,33 @@
  *
  */
 
-(function () {
-  "use strict";
+"use strict";
 
-  var fs = require('fs-extra'),
-  path = require('path');
+var fs = require('fs-extra');
 
-  var pattern_exporter = function(){
+var pattern_exporter = function () {
+  
+  function exportPatterns(patternlab) {
+    //read the config export options
+    var exportKeys = patternlab.config.patternExportKeys;
 
-    function exportPatterns(patternlab){
-
-      //read the config export options
-      var exportKeys = patternlab.config.patternExportKeys;
-
-      //find the chosen patterns to export
-      for (var i = 0; i < exportKeys.length; i++){
-        for (var j = 0; j < patternlab.patterns.length; j++){
-          if(exportKeys[i] === patternlab.patterns[j].key){
-            //write matches to the desired location
-            fs.outputFileSync(patternlab.config.patternExportDirectory + patternlab.patterns[j].key + '.html', patternlab.patterns[j].patternPartial);
-          }
+    //find the chosen patterns to export
+    for (var i = 0; i < exportKeys.length; i++) {
+      for (var j = 0; j < patternlab.patterns.length; j++) {
+        if (exportKeys[i] === patternlab.patterns[j].key) {
+          //write matches to the desired location
+          fs.outputFileSync(patternlab.config.patternExportDirectory + patternlab.patterns[j].key + '.html', patternlab.patterns[j].patternPartial);
         }
       }
     }
+  }
 
-    return {
-      export_patterns: function(patternlab){
-        exportPatterns(patternlab);
-      }
-    };
-
+  return {
+    export_patterns: function (patternlab) {
+      exportPatterns(patternlab);
+    }
   };
 
-  module.exports = pattern_exporter;
+};
 
-}());
+module.exports = pattern_exporter;
--- builder/style_modifier_hunter.js
+++ builder/style_modifier_hunter.js
@@ -8,40 +8,36 @@
  *
  */
 
-(function () {
-	"use strict";
+  "use strict";
 
-	var style_modifier_hunter = function(){
+  var style_modifier_hunter = function () {
 
-		function consumestylemodifier(pattern, partial, patternlab){
+    function consumestylemodifier(pattern, partial, patternlab) {
+      //extract the classname from the stylemodifier which comes in the format of :className
+      var styleModifier = partial.match(/:([\w\-_|])+/g) ? partial.match(/:([\w\-_|])+/g)[0].slice(1) : null;
+      
+      if (styleModifier) {
+        //replace the special character pipe | used to separate multiple classes with a space
+        styleModifier = styleModifier.replace(/\|/g, ' ');
 
-			//extract the classname from the stylemodifier which comes in the format of :className
-			var styleModifier = partial.match(/:([\w\-_|])+/g) ? partial.match(/:([\w\-_|])+/g)[0].slice(1) : null;
-			if(styleModifier){
+        if (patternlab.config.debug) {
+          console.log('found partial styleModifier within pattern ' + pattern.key);
+        }
 
-				//replace the special character pipe | used to separate multiple classes with a space
-				styleModifier = styleModifier.replace(/\|/g, ' ');
+        //replace the stylemodifier placeholder with the class name
+        pattern.extendedTemplate = pattern.extendedTemplate.replace(/{{[ ]?styleModifier[ ]?}}/i, styleModifier);
 
-				if(patternlab.config.debug){
-					console.log('found partial styleModifier within pattern ' + pattern.key);
-				}
+        //update the extendedTemplate in the partials object in case this pattern is consumed later
+        patternlab.partials[pattern.key] = pattern.extendedTemplate;
+      }
+    }
 
-				//replace the stylemodifier placeholder with the class name
-				pattern.extendedTemplate = pattern.extendedTemplate.replace(/{{[ ]?styleModifier[ ]?}}/i, styleModifier);
+    return {
+      consume_style_modifier: function (pattern, partial, patternlab) {
+        consumestylemodifier(pattern, partial, patternlab);
+      }
+    };
 
-				//update the extendedTemplate in the partials object in case this pattern is consumed later
-				patternlab.partials[pattern.key] = pattern.extendedTemplate;
-			}
-		}
+  };
 
-		return {
-			consume_style_modifier: function(pattern, partial, patternlab){
-				consumestylemodifier(pattern, partial, patternlab);
-			}
-		};
-
-	};
-
-	module.exports = style_modifier_hunter;
-
-}());
+  module.exports = style_modifier_hunter;
