c53440a73838fa4bdef97624ae2d8ef42e6cdd71
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2016-03-10 23:46:11 -0600

    add js directory to watch:all grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -88,7 +88,8 @@ module.exports = function (grunt) {
           path.resolve(paths().source.patterns + '**/*.json'),
           path.resolve(paths().source.fonts + '/*'),
           path.resolve(paths().source.images + '/*'),
-          path.resolve(paths().source.data + '*.json')
+          path.resolve(paths().source.data + '*.json'),
+          path.resolve(paths().source.js + '/*.js')
         ],
         tasks: ['default', 'bsReload:css']
       }
