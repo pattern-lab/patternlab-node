14d51704c380d8be074f740bc3a16d1c4d3516aa
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2016-05-23 18:08:50 -0500

    annotations.md parsing with tests

--- test/annotation_exporter_tests.js
+++ test/annotation_exporter_tests.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var eol = require('os').EOL;
 var Pattern = require('../core/lib/object_factory').Pattern;
 var extend = require('util')._extend;
 
@@ -17,25 +18,29 @@ function createFakePatternLab(customProps) {
   return extend(pl, customProps);
 }
 
+var patternlab = createFakePatternLab();
+var ae = require('../core/lib/annotation_exporter')(patternlab);
+
 exports['annotaton_exporter'] = {
 
   'converts old JS annotations into new format': function (test) {
     //arrange
-    var patternlab = createFakePatternLab({
-      "patterns": [
-        {
-          "template": "{{ title }}",
-          "extendedTemplate": "{{ title }}",
-          "patternPartial": "test-simple",
-          "jsonFileData": {}
-        }
-
-      ]
-    });
-    var ae = require('../core/lib/annotation_exporter')(patternlab);
-
     //act
     var annotations = ae.gatherJS();
+
+    //assert
+    test.equals(annotations.length, 2);
+    test.equals(annotations[1].el, '.logo');
+    test.equals(annotations[1].title, 'Logo');
+    test.equals(annotations[1].comment, "The logo image is an SVG file, which ensures that the logo displays crisply even on high resolution displays. A PNG fallback is provided for browsers that don't support SVG images.</p><p>Further reading: <a href=\"http://bradfrostweb.com/blog/mobile/hi-res-optimization/\">Optimizing Web Experiences for High Resolution Screens</a></p>");
+
+    test.done();
+  },
+
+  'converts new markdown annotations into an array': function (test) {
+    //arrange
+    //act
+    var annotations = ae.gatherMD();
 
     console.log(annotations);
 
@@ -43,7 +48,7 @@ exports['annotaton_exporter'] = {
     test.equals(annotations.length, 2);
     test.equals(annotations[1].el, '.logo');
     test.equals(annotations[1].title, 'Logo');
-    test.equals(annotations[1].comment, "The logo image is an SVG file, which ensures that the logo displays crisply even on high resolution displays. A PNG fallback is provided for browsers that don't support SVG images.</p><p>Further reading: <a href=\"http://bradfrostweb.com/blog/mobile/hi-res-optimization/\">Optimizing Web Experiences for High Resolution Screens</a></p>");
+    test.equals(annotations[1].comment.replace(/\r?\n|\r/gm, ""), '<p>The <em>logo image</em> is an SVG file.</p>');
 
     test.done();
   }
--- test/files/annotations.md
+++ test/files/annotations.md
@@ -9,7 +9,4 @@ It's using a linear CSS gradient instead of a background image to give greater d
 selector: ".logo"
 title: "Logo"
 ---
-The _logo image_ is an SVG file, which ensures that the logo displays crisply even on high resolution displays.
-A PNG fallback is provided for browsers that don't support SVG images.
-
-Further reading: [Optimizing Web Experiences for High Resolution Screens](http://bradfrostweb.com/blog/mobile/hi-res-optimization/)
+The _logo image_ is an SVG file.
