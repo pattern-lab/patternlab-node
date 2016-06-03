dc6cd22833fc31b5176356f639389a5a897a8661
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2016-06-03 11:37:49 -0500

    pushed to support https://github.com/pattern-lab/edition-node-gulp/issues/27 once https://github.com/pattern-lab/patternlab-node/issues/347 is fixed

--- core/lib/patternlab.js
+++ core/lib/patternlab.js
@@ -275,7 +275,7 @@ var patternlab_engine = function (config) {
         lineageR: pattern.patternLineagesR,
         patternLineageEExists: pattern.patternLineageExists || pattern.patternLineageRExists,
         patternDesc: pattern.patternDescExists ? pattern.patternDesc : '',
-        patternBreadcrumb: pattern.patternGroup,
+        patternBreadcrumb: pattern.patternGroup + ' > ' + pattern.patternSubGroup,
         patternExtension: pattern.fileExtension,
         patternName: pattern.patternName,
         patternPartial: pattern.patternPartial,
