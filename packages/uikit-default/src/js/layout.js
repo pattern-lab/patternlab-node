34d15e1915588ad9f114355b0e5c50d6e8017cc4
Dave Olsen <dmolsen@gmail.com>
2016-06-27 16:17:09 -0400

    adding a default message if a site hasn't been generated yet

--- packages/uikit-default/src/js/layout.js
+++ packages/uikit-default/src/js/layout.js
@@ -5,14 +5,23 @@
  * Licensed under the MIT license
  */
 
-/* load pattern nav */
-var template         = document.getElementById("pl-pattern-nav-template");
-var templateCompiled = Hogan.compile(template.innerHTML);
-var templateRendered = templateCompiled.render(navItems);
-document.getElementById("pl-pattern-nav-target").innerHTML = templateRendered;
-
-/* load ish controls */
-var template         = document.getElementById("pl-ish-controls-template");
-var templateCompiled = Hogan.compile(template.innerHTML);
-var templateRendered = templateCompiled.render(ishControls);
-document.getElementById("sg-controls").innerHTML = templateRendered;
+try {
+  
+  /* load pattern nav */
+  var template         = document.getElementById("pl-pattern-nav-template");
+  var templateCompiled = Hogan.compile(template.innerHTML);
+  var templateRendered = templateCompiled.render(navItems);
+  document.getElementById("pl-pattern-nav-target").innerHTML = templateRendered;
+  
+  /* load ish controls */
+  var template         = document.getElementById("pl-ish-controls-template");
+  var templateCompiled = Hogan.compile(template.innerHTML);
+  var templateRendered = templateCompiled.render(ishControls);
+  document.getElementById("sg-controls").innerHTML = templateRendered;
+  
+} catch(e) {
+  
+  var message = "<h1>Nothing Here Yet</h1><p>Please generate your site before trying to view it.</p>";
+  document.getElementById("pl-pattern-nav-target").innerHTML = message;
+  
+}
