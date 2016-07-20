c35e4babbeb10ab9ad758dc13014c4cf0d4f76e1
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2016-07-20 00:12:51 -0500

    pattern groups no longer need to be prefixed with two integers and a hyphen closes #388

--- test/object_factory_tests.js
+++ test/object_factory_tests.js
@@ -3,19 +3,20 @@
 
   var of = require('../core/lib/object_factory');
   var Pattern = require('../core/lib/object_factory').Pattern;
+  var path = require('path');
 
 	exports['Pattern initialization'] = {
 		'test Pattern initializes correctly' : function (test) {
 			var p = new Pattern('00-atoms/00-global/00-colors.mustache', { d: 123});
-            test.equals(p.relPath, '00-atoms/00-global/00-colors.mustache');
+      test.equals(p.relPath, '00-atoms' + path.sep + '00-global' + path.sep + '00-colors.mustache');
 			test.equals(p.name, '00-atoms-00-global-00-colors');
-			test.equals(p.subdir, '00-atoms/00-global');
+			test.equals(p.subdir, '00-atoms' + path.sep + '00-global');
 			test.equals(p.fileName, '00-colors');
 			test.equals(p.fileExtension, '.mustache');
 			test.equals(p.jsonFileData.d, 123);
 			test.equals(p.patternBaseName, 'colors');
 			test.equals(p.patternName, 'Colors');
-			test.equals(p.patternLink, '00-atoms-00-global-00-colors/00-atoms-00-global-00-colors.html');
+			test.equals(p.patternLink, '00-atoms-00-global-00-colors' + path.sep + '00-atoms-00-global-00-colors.html');
 			test.equals(p.patternGroup, 'atoms');
 			test.equals(p.patternSubGroup, 'global');
 			test.equals(p.flatPatternPath, '00-atoms-00-global');
@@ -31,7 +32,7 @@
 		},
     'test Pattern with one-directory subdir works as expected' : function (test) {
 		  var p = new Pattern('00-atoms/00-colors.mustache', { d: 123});
-          test.equals(p.relPath, '00-atoms/00-colors.mustache');
+          test.equals(p.relPath, '00-atoms' + path.sep + '00-colors.mustache');
 		  test.equals(p.name, '00-atoms-00-colors');
 		  test.equals(p.subdir, '00-atoms');
 		  test.equals(p.fileName, '00-colors');
@@ -39,7 +40,7 @@
 		  test.equals(p.jsonFileData.d, 123);
 		  test.equals(p.patternBaseName, 'colors');
 		  test.equals(p.patternName, 'Colors');
-		  test.equals(p.patternLink, '00-atoms-00-colors/00-atoms-00-colors.html');
+		  test.equals(p.patternLink, '00-atoms-00-colors' + path.sep + '00-atoms-00-colors.html');
 		  test.equals(p.patternGroup, 'atoms');
 		  test.equals(p.flatPatternPath, '00-atoms');
 		  test.equals(p.patternPartial, 'atoms-colors');
@@ -50,6 +51,18 @@
 		  test.equals(p.lineageRIndex.length, 0);
 		  test.done();
 	    },
+    'test Pattern with no numbers in pattern group works as expected' : function (test) {
+      var p = new Pattern('atoms/colors.mustache', { d: 123});
+      test.equals(p.relPath, 'atoms' + path.sep + 'colors.mustache');
+      test.equals(p.name, 'atoms-colors');
+      test.equals(p.subdir, 'atoms');
+      test.equals(p.fileName, 'colors');
+      test.equals(p.patternLink, 'atoms-colors' + path.sep + 'atoms-colors.html');
+      test.equals(p.patternGroup, 'atoms');
+      test.equals(p.flatPatternPath, 'atoms');
+      test.equals(p.patternPartial, 'atoms-colors');
+      test.done();
+    },
 		'test Pattern capitalizes patternDisplayName correctly' : function(test){
 			var p = new Pattern('00-atoms/00-global/00-colors-alt.mustache', { d: 123});
 			test.equals(p.patternBaseName, 'colors-alt');
