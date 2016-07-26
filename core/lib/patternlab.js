e6fa46faf91133503ab6604fbdda37ec79bc0fc2
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2016-07-25 21:54:42 -0500

    bump version

--- core/lib/patternlab.js
+++ core/lib/patternlab.js
@@ -1,10 +1,10 @@
-/*
- * patternlab-node - v2.2.1 - 2016
- *
+/* 
+ * patternlab-node - v2.3.0 - 2016 
+ * 
  * Brian Muenzenmeyer, Geoff Pursell, and the web community.
- * Licensed under the MIT license.
- *
- * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice.
+ * Licensed under the MIT license. 
+ * 
+ * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice. 
  *
  */
 
