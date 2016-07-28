1a5998797e488896ad6d57003bbfa5f43a3164a4
e2tha-e <ee@electric-eloquence.io>
2016-07-28 10:23:24 -0400

    adding allData to pattern object prototype

--- core/lib/object_factory.js
+++ core/lib/object_factory.js
@@ -57,6 +57,7 @@ var Pattern = function (relPath, data) {
   this.isPseudoPattern = false;
   this.engine = patternEngines.getEngineForPattern(this);
   this.patternPartials = null;
+  this.allData = {};
   this.dataKeys = [];
 };
 
