fde839d74f2ee4063d11c4fe829d4a89900b7cab
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2016-08-14 14:18:45 -0500

    load starterkit on postinstall if found. only supports one for now closes #362

--- core/scripts/postinstall.js
+++ core/scripts/postinstall.js
@@ -1,7 +1,10 @@
 "use strict";
 try{
 
+  console.log('Beginning Pattern Lab postinstall...');
+
   var sm = require('../lib/starterkit_manager.js');
+  var u = require('../lib/utilities.js');
   var path = require('path');
   var fs = require('fs-extra');
 
@@ -13,11 +16,18 @@ try{
   var starterkit_manager = new sm(config);
   var foundStarterkits = starterkit_manager.detect_starterkits();
 
-  console.log(foundStarterkits);
+  //todo - enhance to support multiple kits with prompt for each or all
+  if(foundStarterkits && foundStarterkits.length > 0) {
+    starterkit_manager.load_starterkit(foundStarterkits[0], true);
+  } else {
+    console.log('No starterkits found to automatically load.')
+  }
+  u.logGreen('Pattern Lab postinstall complete.');
 
 } catch (ex) {
-  console.log(ex);
-  console.log('An error occurred during Pattern Lab Node postinstall.');
+  u.logOrange(ex);
+  u.logOrange('An error occurred during Pattern Lab Node postinstall.');
+  u.logOrange('Pattern Lab postinstall completed with errors.');
 }
 
 
