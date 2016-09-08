e0333701100442da2b4e39c1e027db21fd09955e
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2016-09-07 23:34:58 -0500

    install plugin postinstall setting config flag as part of 432

---
+++ core/lib/plugin_manager.js
@@ -0,0 +1,77 @@
+"use strict";
+
+var plugin_manager = function (config, configPath) {
+  var path = require('path'),
+    fs = require('fs-extra'),
+    util = require('./utilities'),
+    paths = config.paths;
+
+  function loadPlugin(pluginName) {
+    var pluginInstance = require(path.join(process.cwd(), 'node_modules', pluginName));
+    return pluginInstance;
+  }
+
+  function installPlugin(pluginName) {
+    try {
+      var pluginPath = path.resolve(
+        path.join(process.cwd(), 'node_modules', pluginName)
+      );
+      console.log('Attempting to load plugin from', pluginPath);
+      try {
+        var pluginDirStats = fs.statSync(pluginPath);
+      } catch (ex) {
+        util.logRed(pluginName + ' not found, please use npm to install it first.');
+        util.logRed(pluginName + ' not loaded.');
+        return;
+      }
+      var pluginPathDirExists = pluginDirStats.isDirectory();
+      if (pluginPathDirExists) {
+
+        //write config entry back
+        var diskConfig = fs.readJSONSync(path.resolve(configPath), 'utf8');
+        diskConfig[pluginName] = true;
+        fs.outputFileSync(path.resolve(configPath), JSON.stringify(diskConfig, null, 2));
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  function detectPlugins() {
+    var node_modules_path = path.join(process.cwd(), 'node_modules');
+    var npm_modules = fs.readdirSync(node_modules_path).filter(function (dir) {
+      var module_path = path.join(process.cwd(), 'node_modules', dir);
+      return fs.statSync(module_path).isDirectory() && dir.indexOf('plugin-node-') === 0;
+    });
+    return npm_modules;
+  }
+
+  function disablePlugin(pluginName) {
+    console.log('not implemented yet');
+  }
+
+  function enablePlugin(pluginName) {
+    console.log('not implemented yet');
+  }
+
+  return {
+    install_plugin: function(pluginName) {
+      installPlugin(pluginName);
+    },
+    load_plugin: function (pluginName) {
+      loadPlugin(pluginName);
+    },
+    detect_plugins: function () {
+      return detectPlugins();
+    },
+    disable_plugin: function (pluginName) {
+      disablePlugin(pluginName);
+    },
+    enable_plugin: function (pluginName) {
+      enablePlugin(pluginName);
+    }
+  };
+
+};
+
+module.exports = plugin_manager;
--- core/scripts/postinstall.js
+++ core/scripts/postinstall.js
@@ -4,6 +4,7 @@ try{
   console.log('Beginning Pattern Lab postinstall...');
 
   var sm = require('../lib/starterkit_manager.js');
+  var pm = require('../lib/plugin_manager.js');
   var u = require('../lib/utilities.js');
   var path = require('path');
   var fs = require('fs-extra');
@@ -17,11 +18,28 @@ try{
   var foundStarterkits = starterkit_manager.detect_starterkits();
 
   //todo - enhance to support multiple kits with prompt for each or all
-  if(foundStarterkits && foundStarterkits.length > 0) {
+  if (foundStarterkits && foundStarterkits.length > 0) {
     starterkit_manager.load_starterkit(foundStarterkits[0], true);
   } else {
     console.log('No starterkits found to automatically load.')
   }
+
+  //determine if any plugins are already installed
+  var plugin_manager = new pm(config, configPath);
+  var foundPlugins = plugin_manager.detect_plugins();
+
+  if (foundPlugins && foundPlugins.length > 0) {
+
+    for (var i = 0; i < foundPlugins.length; i++) {
+      console.log('Found plugin', foundPlugins[i]);
+      plugin_manager.install_plugin(foundPlugins[i]);
+    }
+  }
+
+
+
+
+
   u.logGreen('Pattern Lab postinstall complete.');
 
 } catch (ex) {
