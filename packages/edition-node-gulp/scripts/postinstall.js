a35e21d794587a0fb4aef2636068f4b9c6376f6f
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2016-09-27 08:49:02 -0500

    Merge pull request #80 from pattern-lab/dev
    Edition Node Gulp 1.3.2

---
+++ packages/edition-node-gulp/scripts/postinstall.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('Beginning Pattern Lab Node Gulp postinstall...');
+
+//call the core library postinstall
+var patternlab = require('patternlab-node/core/scripts/postinstall');
