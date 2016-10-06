cdeab805ef412ff48d1e2c45b1ff6fff95a70ca1
Geoffrey Pursell <geoffrey.pursell@target.com>
2016-10-06 13:05:27 -0500

    add matching underscore test

--- test/engine_underscore_tests.js
+++ test/engine_underscore_tests.js
@@ -83,6 +83,26 @@ exports['engine_underscore'] = {
     // test
     test.equals(underscoreEngine.findPartial("<%= _.renderNamedPartial('molecules-details', obj) %>"), 'molecules-details');
     test.done();
+  },
+  'hidden underscore patterns can be called by their nice names' : function(test){
+    const util = require('./util/test_utils.js');
+
+    //arrange
+    const testPatternsPath = path.resolve(__dirname, 'files', '_underscore-test-patterns');
+    const pl = util.fakePatternLab(testPatternsPath);
+    var pattern_assembler = new pa();
+
+    var hiddenPatternPath = path.join('00-atoms', '00-global', '_00-hidden.html');
+    var hiddenPattern = pattern_assembler.process_pattern_iterative(hiddenPatternPath, pl);
+    pattern_assembler.process_pattern_recursive(hiddenPatternPath, pl);
+
+    var testPatternPath = path.join('00-molecules', '00-global', '00-hidden-pattern-tester.html');
+    var testPattern = pattern_assembler.process_pattern_iterative(testPatternPath, pl);
+    pattern_assembler.process_pattern_recursive(testPatternPath, pl);
+
+    //act
+    test.equals(testPattern.render(), 'Here\'s the hidden atom: [I\'m the hidden atom\n]\n');
+    test.done();
   }
 };
 
