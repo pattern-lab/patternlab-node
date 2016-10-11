a84292f81f363f4bf0c27e54598ac740644c2a3c
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2016-10-11 07:46:53 -0500

    removing nodeunit!

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,9 +16,6 @@ module.exports = function (grunt) {
         dest: './core/lib/patternlab.js'
       }
     },
-    nodeunit: {
-      all: ['test/*_tests.js']
-    },
     tape: {
       options: {
         pretty: false,
@@ -37,13 +34,12 @@ module.exports = function (grunt) {
   // load all grunt tasks
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-tape');
 
   //travis CI task
-  grunt.registerTask('travis', ['nodeunit', 'tape', 'eslint']);
+  grunt.registerTask('travis', ['tape', 'eslint']);
 
   //to be run prior to releasing a version
-  grunt.registerTask('build', ['nodeunit', 'tape', 'eslint', 'concat']);
+  grunt.registerTask('build', ['tape', 'eslint', 'concat']);
 
 };
