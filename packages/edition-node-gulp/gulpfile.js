d8f44082b17591958debf6ae7814b0dd47eb1dba
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2016-11-05 22:10:12 -0500

    Merge pull request #90 from pattern-lab/dev
    resolve path with proper concatenation of globs

--- packages/edition-node-gulp/gulpfile.js
+++ packages/edition-node-gulp/gulpfile.js
@@ -41,25 +41,25 @@ gulp.task('pl-copy:font', function(){
 
 // CSS Copy
 gulp.task('pl-copy:css', function(){
-  return gulp.src(resolvePath(paths().source.css, '*.css'))
+  return gulp.src(resolvePath(paths().source.css) + '/*.css')
     .pipe(gulp.dest(resolvePath(paths().public.css)))
     .pipe(browserSync.stream());
 });
 
 // Styleguide Copy everything but css
 gulp.task('pl-copy:styleguide', function(){
-  return gulp.src(resolvePath(paths().source.styleguide, '**/!(*.css)'))
+  return gulp.src(resolvePath(paths().source.styleguide) + '/**/!(*.css)')
     .pipe(gulp.dest(resolvePath(paths().public.root)))
     .pipe(browserSync.stream());
 });
 
 // Styleguide Copy and flatten css
 gulp.task('pl-copy:styleguide-css', function(){
-  return gulp.src(resolvePath(paths().source.styleguide, '**/*.css'))
+  return gulp.src(resolvePath(paths().source.styleguide) + '/**/*.css')
     .pipe(gulp.dest(function(file){
       //flatten anything inside the styleguide into a single output dir per http://stackoverflow.com/a/34317320/1790362
       file.path = path.join(file.base, path.basename(file.path));
-      return resolvePath(path.join(paths().public.styleguide, 'css'));
+      return resolvePath(path.join(paths().public.styleguide, '/css'));
     }))
     .pipe(browserSync.stream());
 });
@@ -141,7 +141,7 @@ function getSupportedTemplateExtensions() {
 }
 function getTemplateWatches() {
   return getSupportedTemplateExtensions().map(function (dotExtension) {
-    return resolvePath(paths().source.patterns, '**/*' + dotExtension);
+    return resolvePath(paths().source.patterns) + '/**/*' + dotExtension;
   });
 }
 
@@ -154,18 +154,20 @@ function reloadCSS() {
 }
 
 function watch() {
-  gulp.watch(resolvePath(paths().source.css, '**/*.css'), { awaitWriteFinish: true }).on('change', gulp.series('pl-copy:css', reloadCSS));
-  gulp.watch(resolvePath(paths().source.styleguide, '**/*.*'), { awaitWriteFinish: true }).on('change', gulp.series('pl-copy:styleguide', 'pl-copy:styleguide-css', reloadCSS));
+  gulp.watch(resolvePath(paths().source.css) + '/**/*.css', { awaitWriteFinish: true }).on('change', gulp.series('pl-copy:css', reloadCSS));
+  gulp.watch(resolvePath(paths().source.styleguide) + '/**/*.*', { awaitWriteFinish: true }).on('change', gulp.series('pl-copy:styleguide', 'pl-copy:styleguide-css', reloadCSS));
 
   var patternWatches = [
-    resolvePath(paths().source.patterns, '**/*.json'),
-    resolvePath(paths().source.patterns, '**/*.md'),
-    resolvePath(paths().source.data, '*.json'),
-    resolvePath(paths().source.fonts + '/*'),
-    resolvePath(paths().source.images + '/*'),
-    resolvePath(paths().source.meta, '*'),
-    resolvePath(paths().source.annotations + '/*')
+    resolvePath(paths().source.patterns) + '/**/*.json',
+    resolvePath(paths().source.patterns) + '/**/*.md',
+    resolvePath(paths().source.data) + '/*.json',
+    resolvePath(paths().source.fonts) + '/*',
+    resolvePath(paths().source.images) + '/*',
+    resolvePath(paths().source.meta) + '/*',
+    resolvePath(paths().source.annotations) + '/*'
   ].concat(getTemplateWatches());
+
+  console.log(patternWatches);
 
   gulp.watch(patternWatches, { awaitWriteFinish: true }).on('change', gulp.series(build, reload));
 }
