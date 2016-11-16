47b3322c04f884cd84a7bc78c10ca47786cb2a48
Raphael Okon <raphael@okon.io>
2016-11-16 21:34:59 +0100

    Adding scaffold test

--- packages/patternlab-node-cli/test/replace_config_paths.test.js
+++ packages/patternlab-node-cli/test/replace_config_paths.test.js
@@ -4,12 +4,14 @@ const config = require('./fixtures/patternlab-config.json');
 
 tap.test('replaceConfigPaths ->', t => {
 	const newConfig = replaceConfigPaths(config, 'projectDir', 'sourceDir', 'publicDir', 'exportDir');
+	console.log(newConfig.patternExportDirectory);
+	
 	for (const k of Object.keys(newConfig.paths.source)) {
 		t.ok(/^projectDir\/sourceDir\/|^node_modules/.test(newConfig.paths.source[k]), `should be ok for newConfig.paths.source.${k}`);
 	}
 	for (const k of Object.keys(newConfig.paths.public)) {
 		t.ok(/^projectDir\/publicDir\//.test(newConfig.paths.public[k]), `should be ok for newConfig.paths.public.${k}`);
 	}
-	t.ok(/^projectDir\/exportDir\//.test(newConfig.paths.patternExportDirectory), `should be ok for newConfig.paths.patternExportDirectory`);
+	t.ok(/^projectDir\/exportDir/.test(newConfig.patternExportDirectory), `should be ok for newConfig.patternExportDirectory`);
 	t.end();
 });
---
+++ packages/patternlab-node-cli/test/scaffold.test.js
@@ -0,0 +1,19 @@
+const tap = require('tap');
+const path = require('path');
+const exists = require('path-exists');
+const scaffold = require('../bin/generator').scaffold;
+const wrapAsync = require('../bin/utils').wrapAsync;
+
+const projectDir = './test/tmp';
+const sourceDir = 'source';
+const publicDir = 'public';
+const exportDir = 'patterns_export';
+
+tap.test('Scaffold ->', t => wrapAsync(function*() {
+	yield scaffold(projectDir, sourceDir, publicDir, exportDir);
+	t.ok(exists.sync(path.resolve(projectDir)), 'should create project dir');
+	t.ok(exists.sync(path.resolve(projectDir, sourceDir)), 'should create source dir');
+	t.ok(exists.sync(path.resolve(projectDir, publicDir)), 'should create public dir');
+	t.ok(exists.sync(path.resolve(projectDir, exportDir)), 'should create export dir');
+	t.end();
+}));
