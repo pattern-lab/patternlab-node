5ed1cd8bdec2619259f1ea45bab33119415b754e
Raphael Okon <raphael@okon.io>
2017-01-01 17:25:36 +0100

    Fixes const binding in replace config test. Fixes #18. Fixes #20

--- packages/patternlab-node-cli/test/replace_config_paths.test.js
+++ packages/patternlab-node-cli/test/replace_config_paths.test.js
@@ -7,8 +7,8 @@ tap.test('replaceConfigPaths ->', t => {
 	for (const k of Object.keys(newConfig.paths.source)) {
 		t.ok(/^projectDir\/sourceDir\/|^node_modules/.test(newConfig.paths.source[k]), `should be ok for newConfig.paths.source.${k}`);
 	}
-	for (const k of Object.keys(newConfig.paths.public)) {
-		t.ok(/^projectDir\/publicDir\//.test(newConfig.paths.public[k]), `should be ok for newConfig.paths.public.${k}`);
+	for (const l of Object.keys(newConfig.paths.public)) {
+		t.ok(/^projectDir\/publicDir\//.test(newConfig.paths.public[l]), `should be ok for newConfig.paths.public.${l}`);
 	}
 	t.ok(/^projectDir\/exportDir/.test(newConfig.patternExportDirectory), `should be ok for newConfig.patternExportDirectory`);
 	t.end();
