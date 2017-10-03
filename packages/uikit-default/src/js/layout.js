005b14f3fddb62bfee62456521de526fe3a3cb08
Brad Frost <brad@bradfrost.com>
2017-10-03 18:04:40 -0400

    Change from IDs to pl-js- classes

--- packages/uikit-default/src/js/layout.js
+++ packages/uikit-default/src/js/layout.js
@@ -8,13 +8,13 @@
 try {
   
   /* load pattern nav */
-  var template         = document.getElementById("pl-pattern-nav-template");
+  var template         = document.querySelector(".pl-js-pattern-nav-template");
   var templateCompiled = Hogan.compile(template.innerHTML);
   var templateRendered = templateCompiled.render(navItems);
   document.querySelector(".pl-js-pattern-nav-target").innerHTML = templateRendered;
   
   /* load ish controls */
-  var template         = document.getElementById("pl-ish-controls-template");
+  var template         = document.querySelector(".pl-js-ish-controls-template");
   var templateCompiled = Hogan.compile(template.innerHTML);
   var templateRendered = templateCompiled.render(ishControls);
   document.querySelector(".pl-js-controls").innerHTML = templateRendered;
