4f675579d4e9f7568733359c85acbdb85b9625b7
Brad Frost <brad@bradfrost.com>
2017-10-05 08:31:14 -0400

    Formatting

--- packages/uikit-default/src/js/prism-languages.js
+++ packages/uikit-default/src/js/prism-languages.js
@@ -6,44 +6,49 @@
  *
  */
 var PrismLanguages = {
-  
-  languages: [],
-  
-  get: function(key) {
-    
-    var language;
-    
-    for (i = 0; i < this.languages.length; ++i) {
-      language = this.languages[i];
-      if (language[key] !== undefined) {
-        return language[key];
-      }
-    }
-    
-    return 'markup';
-    
-  },
-  
-  add: function(language) {
-    
-    // see if the language already exists, overwrite if it does
-    for (var key in language) {
-      if (language.hasOwnProperty(key)) {
-        for (i = 0; i < this.languages.length; ++i) {
-          if (this.languages[i][key] !== undefined) {
-            this.languages[i][key] = language[key];
-            return;
-          }
-        }
-      }
-    }
-    
-    this.languages.push(language);
-    
-  }
-  
+
+	languages: [],
+
+	get: function (key) {
+
+		var language;
+
+		for (i = 0; i < this.languages.length; ++i) {
+			language = this.languages[i];
+			if (language[key] !== undefined) {
+				return language[key];
+			}
+		}
+
+		return 'markup';
+
+	},
+
+	add: function (language) {
+
+		// see if the language already exists, overwrite if it does
+		for (var key in language) {
+			if (language.hasOwnProperty(key)) {
+				for (i = 0; i < this.languages.length; ++i) {
+					if (this.languages[i][key] !== undefined) {
+						this.languages[i][key] = language[key];
+						return;
+					}
+				}
+			}
+		}
+
+		this.languages.push(language);
+
+	}
+
 };
 
 // this shouldn't get hardcoded, also need to think about including Prism's real lang libraries (e.g. handlebars & twig)
-PrismLanguages.add({'twig': 'markup'});
-PrismLanguages.add({'mustache': 'markup'});
+PrismLanguages.add({
+	'twig': 'markup'
+});
+PrismLanguages.add({
+	'mustache': 'markup'
+});
+
