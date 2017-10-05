0571e3ec288b2706498e8a838c612f936f618804
Brad Frost <brad@bradfrost.com>
2017-10-05 08:25:14 -0400

    refactor sg- to pl-; formatting

--- packages/uikit-default/src/js/panels.js
+++ packages/uikit-default/src/js/panels.js
@@ -10,38 +10,62 @@
  */
 
 var Panels = {
-  
-  panels: [],
-  
-  count: function() {
-    return this.panels.length;
-  },
-  
-  get: function() {
-    return JSON.parse(JSON.stringify(this.panels));
-  },
-  
-  add: function(panel) {
-    
-    // if ID already exists in panels array ignore the add()
-    for (i = 0; i < this.panels.length; ++i) {
-      if (panel.id === this.panels[i].id) {
-        return;
-      }
-    }
-    
-    // it wasn't found so push the tab onto the tabs
-    this.panels.push(panel);
-    
-  }
-  
+
+	panels: [],
+
+	count: function () {
+		return this.panels.length;
+	},
+
+	get: function () {
+		return JSON.parse(JSON.stringify(this.panels));
+	},
+
+	add: function (panel) {
+
+		// if ID already exists in panels array ignore the add()
+		for (i = 0; i < this.panels.length; ++i) {
+			if (panel.id === this.panels[i].id) {
+				return;
+			}
+		}
+
+		// it wasn't found so push the tab onto the tabs
+		this.panels.push(panel);
+
+	}
+
 };
 
 // add the default panels
-// Panels.add({ 'id': 'sg-panel-info', 'name': 'info', 'default': true, 'templateID': 'pl-panel-template-info', 'httpRequest': false, 'prismHighlight': false, 'keyCombo': '' });
-// TODO: sort out sg-panel-html
-Panels.add({ 'id': 'sg-panel-pattern', 'name': config.patternExtension.toUpperCase(), 'default': true, 'templateID': 'pl-panel-template-code', 'httpRequest': true, 'httpRequestReplace': '.'+config.patternExtension, 'httpRequestCompleted': false, 'prismHighlight': true, 'language': PrismLanguages.get(config.patternExtension), 'keyCombo': 'ctrl+shift+u' });
-Panels.add({ 'id': 'sg-panel-html', 'name': 'HTML', 'default': false, 'templateID': 'pl-panel-template-code', 'httpRequest': true, 'httpRequestReplace': '.markup-only.html', 'httpRequestCompleted': false, 'prismHighlight': true, 'language': 'markup', 'keyCombo': 'ctrl+shift+y' });
+// Panels.add({ 'id': 'pl-panel-info', 'name': 'info', 'default': true, 'templateID': 'pl-panel-template-info', 'httpRequest': false, 'prismHighlight': false, 'keyCombo': '' });
+// TODO: sort out pl-panel-html
+Panels.add({
+	'id': 'pl-panel-pattern',
+	'name': config.patternExtension.toUpperCase(),
+	'default': true,
+	'templateID': 'pl-panel-template-code',
+	'httpRequest': true,
+	'httpRequestReplace': '.' + config.patternExtension,
+	'httpRequestCompleted': false,
+	'prismHighlight': true,
+	'language': PrismLanguages.get(config.patternExtension),
+	'keyCombo': 'ctrl+shift+u'
+});
+
+Panels.add({
+	'id': 'pl-panel-html',
+	'name': 'HTML',
+	'default': false,
+	'templateID': 'pl-panel-template-code',
+	'httpRequest': true,
+	'httpRequestReplace': '.markup-only.html',
+	'httpRequestCompleted': false,
+	'prismHighlight': true,
+	'language': 'markup',
+	'keyCombo': 'ctrl+shift+y'
+});
 
 // gather panels from plugins
 Dispatcher.trigger('setupPanels');
+
