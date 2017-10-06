db370ebcc7c5bea4d2f119d9df81402c08e1a5d8
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2017-10-06 15:06:40 -0500

    update version number part of #602

--- core/lib/patternlab.js
+++ core/lib/patternlab.js
@@ -1,5 +1,5 @@
 /*
- * patternlab-node - v2.12.0 - 2017
+ * patternlab-node - v3.0.0-alpha.1 - 2017
  *
  * Brian Muenzenmeyer, Geoff Pursell, Raphael Okon, tburny and the web community.
  * Licensed under the MIT license.
