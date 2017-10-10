65f5100a3693772b7caceb460e25eed187379da0
Dan White <dawhite@lcsc.edu>
2017-10-10 15:43:18 -0700

    Continue building...lots of changes

--- packages/patternengine-node-nunjucks/lib/engine_nunjucks.js
+++ packages/patternengine-node-nunjucks/lib/engine_nunjucks.js
@@ -3,34 +3,46 @@
   - Make sure regex match Nunjucks features and syntax
   - Look into if we need to handle partials in the render method
   - Replace 'source/patterns' string with a config variable from PL if possible
+  - Add try catch blocks to prevent Pl from crashing
+  - Test methods of including files
   - Compare features and syntax with the mustache version so we can document
-  - Consider using findPartials and replacing them that way instead of a hard search and replace
-  - Make sure PL links work
   - Document and submit to PatternLab
 */
 
 "use strict";
 
 var _shuffle = require('lodash/shuffle');
-var take = require('lodash/take');
+var _take = require('lodash/take');
 var nunjucks = require('nunjucks');
-// var env = new nunjucks.Environment();
 var env = nunjucks.configure('source/_patterns/');
 var partialRegistry = [];
 
+
+////////////////////////////
+// NUNJUCKS CUSTOMIZATIONS
+////////////////////////////
 env.addFilter('shorten', function (str, count) {
   return str.slice(0, count || 5);
 });
-
-
 env.addFilter('shuffle', (arr) => _shuffle(arr));
-env.addFilter('take', (arr, number) => take(arr, number));
-
-// console.log(env);
-// nunjucks.Environment = env;
+env.addFilter('take', (arr, number) => _take(arr, number));
 
 
+////////////////////////////
+// HELPER FUNCTIONS
+// https://stackoverflow.com/questions/2116558/fastest-method-to-replace-all-instances-of-a-character-in-a-string
+// Might do some research on the solution.
+////////////////////////////
+if (!String.prototype.replaceAll) {
+  String.prototype.replaceAll = function (str1, str2, ignore) {
+    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g, "\\$&"), (ignore ? "gi" : "g")), (typeof (str2) == "string") ? str2.replace(/\$/g, "$$$$") : str2);
+  }
+}
 
+
+////////////////////////////
+// NUNJUCKS ENGINE
+////////////////////////////
 var engine_nunjucks = {
   engine: nunjucks,
   engineName: 'nunjucks',
@@ -40,58 +52,61 @@ var engine_nunjucks = {
   expandPartials: false,
 
   // regexes, stored here so they're only compiled once
-  findPartialsRE: /{%\s*(?:extends|include|import|from)\s+('[^']+'|"[^"]+").*?%}/g,
-  findPartialKeyRE: /"((?:\\.|[^"\\])*)"/,
-  findPartialNameRE: /(['"])([^"'\s]*[^"'\s]*)\1/,
+  findPartialsRE: /{%\s*(?:extends|include|import|from)\s+(?:.*)\s*%}/g,
+  findPartialKeyRE: /{%\s*(?:extends|include|import|from)\s+('[^']+'|"[^"]+")/,
 
   // render it
   renderPattern: function renderPattern(pattern, data) {
     // replace pattern names with their full path so Nunjucks can find them.
-    for (var i = 0; i < partialRegistry.length; i++) {
-      pattern.extendedTemplate = pattern.extendedTemplate.split(partialRegistry[i].key).join(partialRegistry[i].value);
-    }
-
-    var partials = this.findPartials(pattern);
-
-    if (partials !== null) {
-      // console.log('Partial: ' + this.findPartial(partials[i]));
-      for (var i = 0; i < partials.length; i++) {
-        // console.log('Partial: ' + partials[i].match(this.findPartialNameRE)[2]);
-        console.log('Partial: ' + this.findPartial(partials[i]));
-      }
-    }
-
-    var result = nunjucks.renderString(pattern.extendedTemplate, data);
-
+    pattern.extendedTemplate = this.replacePartials(pattern);
+    // var result = nunjucks.renderString(pattern.extendedTemplate, data);
+    var result = nunjucks.compile(pattern.extendedTemplate, env).render(data);
     return result;
   },
 
-  // find and return any {% include|extends|import 'template-name' %} within pattern
+  // find and return any Nunjucks style includes/imports/extends within pattern
+  // EXAMPLES OF NUNJUCKS STYLE PARTIAL INCLUDES
+  // {% include "atoms-parent" %} [WORKS]
+  // {% include 'atoms-parent' %}
+  // {% include "atoms-parent" ignore missing %}
+  // {% import "atoms-parent" as forms %} [WORKS]
+  // {% from "atoms-parent" import field, label as description %} [WORKS]
+  // {% extends "atoms-parent" %} [WORKS]
+  // {% extends parentTemplate %}
+  // {% extends name + ".njk" %}
   findPartials: function findPartials(pattern) {
     var matches = pattern.template.match(this.findPartialsRE);
+    console.log('Matches: ' + matches);
     return matches;
   },
 
   // given a pattern, and a partial string, tease out the "pattern key" and return it.
   findPartial: function (partialString) {
-    var partial = partialString.match(this.findPartialKeyRE)[0];
+    var partial = partialString.match(this.findPartialKeyRE)[1];
     partial = partial.replace(/"/g, '');
+    console.log('Partial: ' + partial);
     return partial;
   },
 
   // keep track of partials and their paths so we can replace the name with the path
   registerPartial: function (pattern) {
     // only register each partial once. Otherwise we'll eat up a ton of memory.
-    var found = partialRegistry.find(function (o) {
-      return o.key === pattern.patternPartial;
-    });
-
-    if (found === undefined) {
-      partialRegistry.push({
-        key: pattern.patternPartial,
-        value: pattern.relPath.split('\\').join('/')
-      });
+    if (partialRegistry.indexOf(pattern.patternPartial) === -1) {
+      partialRegistry[pattern.patternPartial] = pattern.relPath.replace(/\\/g, '/');
     }
+  },
+
+  replacePartials: function (pattern) {
+    var partials = this.findPartials(pattern);
+    if (partials !== null) {
+      for (var i = 0; i < partials.length; i++) { // e.g. {% include "atoms-parent" %}
+        var partialName = this.findPartial(partials[i]); // e.g. atoms-parent
+        var partialFullPath = partialRegistry[partialName]; // e.g. 00-atoms/01-parent.njk
+        var newPartial = partials[i].replaceAll(partialName, partialFullPath, true); // e.g. {% include "00-atoms/01-parent.njk" %}
+        pattern.extendedTemplate = pattern.extendedTemplate.replaceAll(partials[i], newPartial, true);
+      }
+    }
+    return pattern.extendedTemplate;
   },
 
   // handled by nunjucks. This is error to keep PL from erroring
