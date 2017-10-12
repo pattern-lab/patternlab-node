c198aad033d39715aa4f7c2b27ca76e63b558318
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2017-10-12 14:03:16 -0500

    first draft of @raphaelokon's custom live-server part of #609

--- core/lib/serve.js
+++ core/lib/serve.js
@@ -1,14 +1,38 @@
 "use strict";
-const plutils = require('./utilities');
 const path = require('path');
-const process = require('process');
-const assetCopier = require('./asset_copy');
-
-let copy = require('recursive-copy'); // eslint-disable-line
-let chokidar = require('chokidar'); // eslint-disable-line
+const liveServer = require('live-server');
 
 const serve = (patternlab) => {
-  console.log('serve');
+
+  // our default liveserver config
+  const defaults = {
+    root: patternlab.config.paths.public.root,
+    open: true,
+    ignore: path.join(path.resolve(patternlab.config.paths.public.root)),
+    file: 'index.html',
+    logLevel: 0 // errors only
+  };
+
+  // allow for overrides should they exist inside patternlab-config.json
+  const liveServerConfig = Object.assign({}, defaults, patternlab.config.serverOptions);
+
+  // watch for asset changes, and reload appropriately
+  patternlab.events.on('patternlab-asset-change', (data) => {
+    if (data.file.indexOf('css') > -1) {
+      liveServer.refreshCSS();
+    } else {
+      liveServer.reload();
+    }
+  });
+
+  //watch for pattern changes, and reload
+  patternlab.events.on('patternlab-pattern-change', () => {
+    liveServer.reload();
+  });
+
+  //start!
+  liveServer.start(liveServerConfig);
+
 };
 
 module.exports = serve;
