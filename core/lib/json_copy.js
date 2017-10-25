4a681a49b7d73e43f193e0e8176dacbdcedb159d
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2017-10-25 05:01:17 -0500

    fix(json_copy): Warn on error and rethrow to caller. They may be able to recover

--- core/lib/json_copy.js
+++ core/lib/json_copy.js
@@ -6,8 +6,8 @@ const json_copy = (data, callee) => {
     return JSON.parse(JSON.stringify(data));
   } catch (e) {
     //this is unlikely to be hit due to the passed in data already being loaded using JSON parsers
-    plutils.error(`JSON provided by ${callee} is invalid and cannot be copied`);
-    return {};
+    plutils.warning(`JSON provided by ${callee} is invalid and cannot be copied`);
+    throw (e);
   }
 };
 
