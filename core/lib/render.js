558967e9053b787996efdabd67c85ef6805594fd
Brian Muenzenmeyer <Brian.Muenzenmeyer@target.com>
2017-12-22 08:50:08 -0600

    feat(render): Add debugging

--- core/lib/render.js
+++ core/lib/render.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const logger = require('./log');
+
 module.exports = function (pattern, data, partials) {
+  logger.info(`render: ${(pattern.patternPartial !== '-.' ? pattern.patternPartial : 'ad hoc partial with template' + pattern.extendedTemplate)}`);
   return pattern.render(data, partials);
 };
