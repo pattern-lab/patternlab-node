c706267dd66df50a9982c590f9cd51d6e8d5919c
Brian Muenzenmeyer <Brian.Muenzenmeyer@target.com>
2018-01-13 00:20:24 -0600

    refactor(processRecursive): Use getPartial

--- core/lib/processRecursive.js
+++ core/lib/processRecursive.js
@@ -2,27 +2,18 @@
 
 const logger = require('./log');
 const decompose = require('./decompose');
+const getPartial = require('./get');
 
 module.exports = function (file, patternlab) {
 
-  console.log(8, file)
-
   //find current pattern in patternlab object using file as a partial
-  var currentPattern, i;
-
-  for (i = 0; i < patternlab.patterns.length; i++) {
-    if (patternlab.patterns[i].relPath === file) {
-      currentPattern = patternlab.patterns[i];
-    }
-  }
+  var currentPattern = getPartial(file, patternlab);
 
   //return if processing an ignored file
   if (typeof currentPattern === 'undefined') { return Promise.resolve(); }
 
   //we are processing a markdown only pattern
   if (currentPattern.engine === null) { return Promise.resolve(); }
-
-  console.log(25, currentPattern.patternPartial)
 
   //call our helper method to actually unravel the pattern with any partials
   return decompose(currentPattern, patternlab)
