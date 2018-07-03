4ae13ce99ea17ffa0ab48a0f6370a5b70834d6d3
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2018-07-03 06:24:03 -0500

    fix(cli): support scoped plugins

--- packages/cli/bin/install-plugin.js
+++ packages/cli/bin/install-plugin.js
@@ -6,10 +6,24 @@ const wrapAsync = require('./utils').wrapAsync;
 const installPlugin = (plugin, config) =>
 	wrapAsync(function*() {
 		const name = plugin.name || plugin;
-		yield checkAndInstallPackage(name);
+		const safePluginName = _.kebabCase(name);
+		yield checkAndInstallPackage(safePluginName);
 		// Put the installed plugin in the patternlab-config.json
-		_.set(config, `plugins[${name}]['enabled']`, false);
-		return name;
+		_.set(config, `plugins[${safePluginName}]['enabled']`, true);
+
+		// Get the options from the plugin, if any
+		const pluginPathConfig = path.resolve(
+			path.join(process.cwd(), 'node_modules', safePluginName, 'config.json')
+		);
+		try {
+			const pluginConfigJSON = require(pluginPathConfig);
+			if (!_.has(config.plugins[safePluginName].options)) {
+				_.set(config, `plugins[${safePluginName}]['options]`, pluginConfigJSON);
+			}
+		} catch (ex) {
+			//a config.json file is not required at this time
+		}
+		return safePluginName;
 	});
 
 module.exports = installPlugin;
