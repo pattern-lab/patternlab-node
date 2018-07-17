a1a97799dea15e8ef6836f7a527e081644c73a50
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2018-07-17 00:00:02 -0500

    fix(postinstall): fix typo in name

--- packages/plugin-tab/postinstall.js
+++ packages/plugin-tab/postinstall.js
@@ -1,5 +1,5 @@
 console.log(`Pattern Lab Node Plugin - "plugin-tab" installed.
-Use the CLI to install this: patternlab install --plugins @pattern-lab/tab
+Use the CLI to install this: patternlab install --plugins @pattern-lab/plugin-tab
 Configure or disable this plugin inside your patternlab-config.json file.
 Add tabs to the Pattern Lab UI by adding file extensions to the "tabsToAdd" array on the plugins['@pattern-lab/plugin-tab'].options object.
 `);
