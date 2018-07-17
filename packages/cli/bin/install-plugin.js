0abfb7b01d3e39797842e91c9923ba1787d120ca
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2018-07-16 23:35:25 -0500

    chore(cli): remove kebabbing of plugin name

--- packages/cli/bin/install-plugin.js
+++ packages/cli/bin/install-plugin.js
@@ -10,10 +10,9 @@ const wrapAsync = require('./utils').wrapAsync;
 const installPlugin = (plugin, config) =>
   wrapAsync(function*() {
     const name = plugin.name || plugin;
-    const safePluginName = _.kebabCase(name);
     yield checkAndInstallPackage(name);
     // Put the installed plugin in the patternlab-config.json
-    _.set(config, `plugins[${safePluginName}]['enabled']`, true);
+    _.set(config, `plugins[${name}]['enabled']`, true);
 
     // Get the options from the plugin, if any
     const pluginPathConfig = path.resolve(
@@ -21,13 +20,13 @@ const installPlugin = (plugin, config) =>
     );
     try {
       const pluginConfigJSON = require(pluginPathConfig);
-      if (!_.has(config.plugins[safePluginName].options)) {
-        _.set(config, `plugins[${safePluginName}]['options]`, pluginConfigJSON);
+      if (!_.has(config.plugins[name].options)) {
+        _.set(config, `plugins[${name}]['options]`, pluginConfigJSON);
       }
     } catch (ex) {
       //a config.json file is not required at this time
     }
-    return safePluginName;
+    return name;
   });
 
 module.exports = installPlugin;
