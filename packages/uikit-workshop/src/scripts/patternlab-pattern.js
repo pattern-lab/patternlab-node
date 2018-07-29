464ee47e9ecd75268aed2f70b8e9303056839db5
Salem Ghoweri <me@salemghoweri.com>
2018-07-29 06:46:51 -0400

    refactor: add new entrypoint files for patternlab-pattern and patternlab-viewer JS

---
+++ packages/uikit-workshop/src/scripts/patternlab-pattern.js
@@ -0,0 +1,6 @@
+import './utils/postmessage';
+import './components/panels-util';
+import './components/modal-styleguide';
+
+// WIP: new JS components for handling UI controls to toggle theme
+// import './components/toggle-theme-sg';
