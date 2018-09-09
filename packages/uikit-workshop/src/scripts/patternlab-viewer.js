1537c4214e888a3d4d9508a181185665d88118f3
Salem Ghoweri <me@salemghoweri.com>
2018-09-09 08:28:44 -0400

    chore: remove theming and layout toggles now handled by <pl-toggle-theme> and <pl-toggle-layout> custom elements

--- packages/uikit-workshop/src/scripts/patternlab-viewer.js
+++ packages/uikit-workshop/src/scripts/patternlab-viewer.js
@@ -7,10 +7,6 @@ import './components/pattern-finder';
 import './components/plugin-loader';
 import './components/styleguide';
 
-// WIP: new JS components for handling UI controls to toggle theme + layout
-// import './components/toggle-layout';
-// import './components/toggle-theme';
-
 //// Add hook to auto re-render the root component.
 if (typeof module.hot === 'object') {
   module.hot.accept(err => {
