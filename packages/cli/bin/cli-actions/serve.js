663d8e185efd951ae67a37e3ec97f76d6cec0d5e
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2018-09-14 10:22:43 -0500

    fix(cli): do not call build before serve
    serve calls build. with both of these running, they both setup watches and conflict with each other
    
    fixes #917

--- packages/cli/bin/cli-actions/serve.js
+++ packages/cli/bin/cli-actions/serve.js
@@ -1,13 +1,12 @@
 'use strict';
 const resolveConfig = require('../resolve-config');
-const build = require('./build');
 const servePatterns = require('../serve');
 const wrapAsync = require('../utils').wrapAsync;
 
 const serve = options =>
   wrapAsync(function*() {
     const config = yield resolveConfig(options.parent.config);
-    yield build(options);
+    servePatterns(config, options);
     servePatterns(config, options.watch);
   });
 
