73d39cec3d56fe242326f93d059aae6857ec9bc1
Salem Ghoweri <me@salemghoweri.com>
2018-09-15 12:36:09 -0400

    chore: fix background color of mobile nav on smaller screens; fresh prod build

---
+++ packages/uikit-workshop/dist/styleguide/js/2-chunk-e07784faf6fb566e89ab.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],{
+
+/***/ "../../../../node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js":
+/*!****************************************************************************************************************!*\
+  !*** /Users/sghoweri/sites/bolt-v2/node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js ***!
+  \****************************************************************************************************************/
+/*! no static exports found */
+/*! ModuleConcatenation bailout: Module is not an ECMAScript module */
+/***/ (function(module, exports) {
+
+eval("/**\n@license @nocompile\nCopyright (c) 2018 The Polymer Project Authors. All rights reserved.\nThis code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt\nThe complete set of authors may be found at http://polymer.github.io/AUTHORS.txt\nThe complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt\nCode distributed by Google as part of the polymer project is also\nsubject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt\n*/\n(function () {\n  'use strict';\n\n  (function () {\n    if (void 0 === window.Reflect || void 0 === window.customElements || window.customElements.hasOwnProperty('polyfillWrapFlushCallback')) return;\n    var a = HTMLElement;\n    window.HTMLElement = function () {\n      return Reflect.construct(a, [], this.constructor);\n    }, HTMLElement.prototype = a.prototype, HTMLElement.prototype.constructor = HTMLElement, Object.setPrototypeOf(HTMLElement, a);\n  })();\n})();\n\n//# sourceURL=webpack:////Users/sghoweri/sites/bolt-v2/node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js?");
+
+/***/ })
+
+}]);
