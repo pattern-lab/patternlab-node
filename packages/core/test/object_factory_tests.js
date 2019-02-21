b30ed5c30b9a63eeffeae6b9a527ccc07ac8fecb
Josef Bredereck <josef.bredereck@acando.de>
2019-02-21 11:58:48 +0100

    Added separated pattern-folder support
    Added the functionality to separate the patterns from each other by adding the support for a capsulation folder by pattern. Also added tests.

--- packages/core/test/object_factory_tests.js
+++ packages/core/test/object_factory_tests.js
@@ -63,6 +63,66 @@ tap.test('test Pattern initializes correctly', function(test) {
   test.end();
 });
 
+tap.test(
+  'test Pattern initializes correctly with pattern in sepatated directory',
+  function(test) {
+    var p = new Pattern('00-atoms/00-global/00-colors/colors.mustache', {
+      d: 123,
+    });
+    test.equals(
+      p.relPath,
+      '00-atoms' +
+        path.sep +
+        '00-global' +
+        path.sep +
+        '00-colors' +
+        path.sep +
+        'colors.mustache'
+    );
+    test.equals(p.name, '00-atoms-00-global-00-colors');
+    test.equals(
+      p.subdir,
+      '00-atoms' + path.sep + '00-global' + path.sep + '00-colors'
+    );
+    test.equals(p.fileName, 'colors');
+    test.equals(p.fileExtension, '.mustache');
+    test.equals(p.jsonFileData.d, 123);
+    test.equals(p.patternBaseName, 'colors');
+    test.equals(p.patternName, 'Colors');
+    test.equals(
+      p.getPatternLink(pl),
+      '00-atoms-00-global-00-colors' +
+        path.sep +
+        '00-atoms-00-global-00-colors.rendered.html'
+    );
+    test.equals(p.patternGroup, 'atoms');
+    test.equals(p.patternSubGroup, 'global');
+    test.equals(p.flatPatternPath, '00-atoms-00-global');
+    test.equals(p.patternPartial, 'atoms-colors');
+    test.equals(p.template, '');
+    test.equals(p.patternPartialCode, '');
+    test.equals(p.lineage.length, 0);
+    test.equals(p.lineageIndex.length, 0);
+    test.equals(p.lineageR.length, 0);
+    test.equals(p.lineageRIndex.length, 0);
+    test.equals(p.patternState, '');
+    test.end();
+  }
+);
+
+tap.test('test Pattern name for variants correctly initialzed', function(test) {
+  var p1 = new Pattern('00-atoms/00-global/00-colors/colors~variant.mustache', {
+    d: 123,
+  });
+  var p2 = new Pattern(
+    '00-atoms/00-global/00-colors/colors~variant-minus.json',
+    { d: 123 }
+  );
+  test.equals(p1.name, '00-atoms-00-global-00-colors-variant');
+  test.equals(p2.name, '00-atoms-00-global-00-colors-variant-minus');
+  test.end();
+});
+
 tap.test('test Pattern with one-directory subdir works as expected', function(
   test
 ) {
@@ -118,6 +178,49 @@ tap.test('test Pattern capitalizes patternDisplayName correctly', function(
   test.end();
 });
 
+tap.test('test Pattern get dir level no sepatated pattern directory', function(
+  test
+) {
+  var p = new Pattern('00-atoms/00-global/00-colors-alt.mustache', { d: 123 });
+  console.log(p);
+  test.equals(p.getDirLevel(0, { hasDir: false, dirLevel: 2 }), '00-atoms');
+  test.equals(p.getDirLevel(1, { hasDir: false, dirLevel: 2 }), '00-global');
+  test.equals(p.getDirLevel(3, { hasDir: false, dirLevel: 2 }), '');
+  var p = new Pattern('00-atoms/00-colors-alt.mustache', { d: 123 });
+  test.equals(p.getDirLevel(0, { hasDir: false, dirLevel: 1 }), '00-atoms');
+  test.equals(p.getDirLevel(1, { hasDir: false, dirLevel: 1 }), '00-atoms');
+  test.equals(p.getDirLevel(3, { hasDir: false, dirLevel: 1 }), '');
+  var p = new Pattern('00-colors-alt.mustache', { d: 123 });
+  test.equals(p.getDirLevel(0, { hasDir: false, dirLevel: 0 }), '');
+  test.equals(p.getDirLevel(1, { hasDir: false, dirLevel: 0 }), '');
+  test.equals(p.getDirLevel(3, { hasDir: false, dirLevel: 0 }), '');
+  test.end();
+});
+
+tap.test(
+  'test Pattern get dir level with sepatated pattern directory',
+  function(test) {
+    var p = new Pattern(
+      '00-atoms/00-global/00-colors-alt/colors-alt.mustache',
+      { d: 123 }
+    );
+    test.equals(p.getDirLevel(0, { hasDir: true, dirLevel: 3 }), '00-atoms');
+    test.equals(p.getDirLevel(1, { hasDir: true, dirLevel: 3 }), '00-global');
+    test.equals(p.getDirLevel(3, { hasDir: true, dirLevel: 3 }), '');
+    var p = new Pattern('00-atoms/00-colors-alt/colors-alt.mustache', {
+      d: 123,
+    });
+    test.equals(p.getDirLevel(0, { hasDir: true, dirLevel: 2 }), '00-atoms');
+    test.equals(p.getDirLevel(1, { hasDir: true, dirLevel: 2 }), '00-atoms');
+    test.equals(p.getDirLevel(3, { hasDir: true, dirLevel: 2 }), '');
+    var p = new Pattern('00-colors-alt/colors-alt.mustache', { d: 123 });
+    test.equals(p.getDirLevel(0, { hasDir: true, dirLevel: 1 }), '');
+    test.equals(p.getDirLevel(1, { hasDir: true, dirLevel: 1 }), '');
+    test.equals(p.getDirLevel(3, { hasDir: true, dirLevel: 1 }), '');
+    test.end();
+  }
+);
+
 tap.test('The forms of Pattern.getPatternLink() work as expected', function(
   test
 ) {
@@ -145,6 +248,12 @@ tap.test('The forms of Pattern.getPatternLink() work as expected', function(
     '00-atoms-00-global-00-colors' +
       path.sep +
       '00-atoms-00-global-00-colors.markup-only.html'
+  );
+  test.equals(
+    p.getPatternLink(pl, 'custom', '.custom-extension'),
+    '00-atoms-00-global-00-colors' +
+      path.sep +
+      '00-atoms-00-global-00-colors.custom-extension'
   );
   test.end();
 });
