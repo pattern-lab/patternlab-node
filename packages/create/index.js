416ba0e50fe7e9a75d2b3f4a6d41b7bcc0c2da5b
Evan Lovely <evanlovely@gmail.com>
2019-04-09 17:09:30 -0700

    fix(create): deprecating @pattern-lab/create in favor of create-pattern-lab

--- packages/create/index.js
+++ packages/create/index.js
@@ -1,4 +1,8 @@
 #! /usr/bin/env node
+console.log(
+  'This is deprecated; please use "npm create pattern-lab" now, which is the `create-pattern-lab` package. Have a nice day!'
+);
+process.exit(1);
 const init = require('@pattern-lab/cli/bin/cli-actions/init');
 
 init({});
