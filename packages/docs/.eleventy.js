e61ab9f50c32086f9d60a63f12e038c0cf3c46e1
Andy Bell <me@andy-bell.co.uk>
2019-06-18 22:59:15 +0100

    Add syntax highlighting

--- packages/docs/.eleventy.js
+++ packages/docs/.eleventy.js
@@ -1,3 +1,55 @@
-const config = require('./config.js');
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
-module.exports = config;
+// Import filters
+const dateFilter = require('./src/filters/date-filter.js');
+const markdownFilter = require('./src/filters/markdown-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
+// Import transforms
+const htmlMinTransform = require('./src/transforms/html-min-transform.js');
+const parseTransform = require('./src/transforms/parse-transform.js');
+
+// Import data files
+const site = require('./src/_data/site.json');
+
+module.exports = function(config) {
+  // Filters
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('markdownFilter', markdownFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
+
+  // Layout aliases
+  config.addLayoutAlias('home', 'layouts/home.njk');
+
+  // Transforms
+  config.addTransform('htmlmin', htmlMinTransform);
+  config.addTransform('parse', parseTransform);
+
+  // Passthrough copy
+  config.addPassthroughCopy('src/fonts');
+  config.addPassthroughCopy('src/admin/config.yml');
+
+  // Custom collections
+  config.addCollection('posts', collection => {
+    return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+  });
+
+  config.addCollection('postFeed', collection => {
+    return [...collection.getFilteredByGlob('./src/posts/*.md')]
+      .reverse()
+      .slice(0, site.maxPostsPerPage);
+  });
+
+  // Plugins
+  config.addPlugin(rssPlugin);
+  config.addPlugin(syntaxHighlight);
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist'
+    },
+    passthroughFileCopy: true
+  };
+};
