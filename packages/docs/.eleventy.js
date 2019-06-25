075f106698271dd7ac8c3844110f0ada37cd8c55
Andy Bell <me@andy-bell.co.uk>
2019-06-25 20:14:11 +0100

    Pre-release tweaks

--- packages/docs/.eleventy.js
+++ packages/docs/.eleventy.js
@@ -38,16 +38,13 @@ module.exports = function(config) {
   // Custom collections
   const livePosts = post => post.date <= now && !post.data.draft;
   config.addCollection('posts', collection => {
-    return collection
-      .getFilteredByGlob('./src/posts/*.md')
-      .filter(livePosts)
-      .reverse();
+    return [
+      ...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)
+    ].reverse();
   });
 
   config.addCollection('postFeed', collection => {
-    return collection
-      .getFilteredByGlob('./src/posts/*.md')
-      .filter(livePosts)
+    return [...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)]
       .reverse()
       .slice(0, site.maxPostsPerPage);
   });
