623e6d7ff2c21284d92c803b6062061f3faf10a8
Andy Bell <me@andy-bell.co.uk>
2019-07-16 21:36:35 +0100

    Merge pull request #60 from brandonaaron/exclude-scheduled-posts
    Exclude future posts from tag lists

--- packages/docs/src/_data/global.js
+++ packages/docs/src/_data/global.js
@@ -4,5 +4,6 @@ module.exports = {
       return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
     };
     return `${segment()}-${segment()}-${segment()}`;
-  }
+  },
+  now: Date.now()
 };
