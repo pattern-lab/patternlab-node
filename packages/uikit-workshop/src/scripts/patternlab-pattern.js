9d602fe335a5d3b5bca5cac258c2465934d9a46a
Salem Ghoweri <me@salemghoweri.com>
2019-10-20 11:14:38 -0400

    feat: add support for auto-closing Nav when clicking inside of the rendered iframe

--- packages/uikit-workshop/src/scripts/patternlab-pattern.js
+++ packages/uikit-workshop/src/scripts/patternlab-pattern.js
@@ -2,4 +2,5 @@ import './utils/polyfills/custom-event-polyfill';
 import './utils/polyfills/symbol-polyfill';
 import './components/modal-styleguide';
 import './components/pl-search/pl-search.iframe-helper';
+import './components/pl-nav/pl-nav.iframe-helper';
 import './utils/share-inner-iframe-data';
