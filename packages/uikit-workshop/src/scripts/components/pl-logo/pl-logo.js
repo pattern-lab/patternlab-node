aff1777168f4c54d189f1638a26d61397af14e6a
Salem Ghoweri <me@salemghoweri.com>
2019-10-20 11:05:49 -0400

    refactor: update Logo component to allow deeper customizations + switch to lit-html for rendering

--- packages/uikit-workshop/src/scripts/components/pl-logo/pl-logo.js
+++ packages/uikit-workshop/src/scripts/components/pl-logo/pl-logo.js
@@ -1,12 +1,12 @@
 import { define, props } from 'skatejs';
-import { h } from 'preact';
 import { store } from '../../store.js'; // connect to redux
 
 const classNames = require('classnames');
-import { BaseComponent } from '../base-component.js';
+import { html } from 'lit-html';
+import { BaseLitComponent } from '../base-component.js';
 
 @define
-class Logo extends BaseComponent {
+class Logo extends BaseLitComponent {
   static is = 'pl-logo';
 
   constructor(self) {
@@ -16,28 +16,39 @@ class Logo extends BaseComponent {
 
   connected() {
     const state = store.getState();
-    this.themeMode = state.app.themeMode || 'dark';
-  }
-
-  shouldUpdate(prevProps, prevState) {
-    return true;
+    this.theme = this.theme || state.app.themeMode || 'dark';
   }
 
   static props = {
+    ratio: props.string,
+    theme: props.string,
     url: props.string,
     text: props.string,
-    src: props.string,
+    altText: props.string,
+    srcLight: props.string,
+    srcDark: props.string,
   };
 
-  render({ themeMode }) {
-    return (
-      <a href={this.props.url} className="pl-c-logo">
-        <img src={this.props.src} className="pl-c-logo__img" />
-        {this.props.text && (
-          <span className="pl-c-logo__text">{this.props.text}</span>
-        )}
+  render() {
+    const imageSrc = this.theme === 'dark' ? this.srcDark : this.srcLight;
+
+    return html`
+      <a
+        href="${this.props.url === '' ? undefined : this.props.url}"
+        class="pl-c-logo"
+      >
+        <img
+          alt=${this.props.altText || 'Pattern Lab Logo'}
+          src=${imageSrc}
+          class="pl-c-logo__img"
+        />
+        ${this.props.text && this.props.text !== ''
+          ? html`
+              <span class="pl-c-logo__text">{this.props.text}</span>
+            `
+          : ''}
       </a>
-    );
+    `;
   }
 }
 
