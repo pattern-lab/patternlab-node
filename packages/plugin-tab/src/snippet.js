b82bd129fdbe48de95b62d75fb7fe95cea896b7e
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2019-11-12 16:06:13 -0600

    fix(plugin-tab): defensively call addPanels

--- packages/plugin-tab/src/snippet.js
+++ packages/plugin-tab/src/snippet.js
@@ -1,14 +1,22 @@
-/* global Panels */
+function addPanels() {
+  window.patternlab.panels.add({
+    id: 'sg-panel-scss',
+    name: 'SCSS',
+    default: false,
+    templateID: 'pl-panel-template-code',
+    httpRequest: true,
+    httpRequestReplace: '.scss',
+    httpRequestCompleted: false,
+    prismHighlight: true,
+    language: 'scss',
+    keyCombo: 'ctrl+shift+z',
+  });
+}
 
-window.patternlab.panels.add({
-  id: 'sg-panel-<<type>>',
-  name: '<<typeUC>>',
-  default: false,
-  templateID: 'pl-panel-template-code',
-  httpRequest: true,
-  httpRequestReplace: '.<<type>>',
-  httpRequestCompleted: false,
-  prismHighlight: true,
-  language: '<<type>>',
-  keyCombo: 'ctrl+shift+z',
-});
+if (window.patternlab && window.patternlab.panels) {
+  console.log('pl already ready');
+  addPanels();
+} else {
+  console.log('waiting for PL to be ready');
+  document.addEventListener('patternLab.pageLoad', addPanels);
+}
