789128bdafd5570b3b298af1b7f29729e970ae17
Hydraner <drupal@pascalcrott.del>
2020-03-17 18:05:01 +0100

    Implement exclude tags feature.

--- packages/core/src/lib/uikitExcludePattern.js
+++ packages/core/src/lib/uikitExcludePattern.js
@@ -1,7 +1,11 @@
 'use strict';
 
+var _ = require('lodash');
+
 const uikitExcludePattern = (pattern, uikit) => {
   const state = pattern.patternState;
-  return uikit.excludedPatternStates.includes(state);
+  const tags = pattern.tags;
+
+  return uikit.excludedPatternStates.includes(state) || _.intersection(uikit.excludedTags, tags).length > 0;
 };
 module.exports = uikitExcludePattern;
--- packages/core/test/uikitExcludePattern_tests.js
+++ packages/core/test/uikitExcludePattern_tests.js
@@ -50,4 +50,51 @@ tap.test(
     test.true(result);
     test.end();
   }
+
+tap.test(
+  'uikitExcludePattern - returns false when uikit has no excluded tags',
+  test => {
+    //arrange
+    const uikit = { excludedTags: [] };
+    const pattern = { tags: 'foo-tag' };
+
+    //act
+    const result = uikitExcludePattern(pattern, uikit);
+
+    //assert
+    test.false(result);
+    test.end();
+  }
+);
+
+tap.test(
+  'uikitExcludePattern - returns false pattern does not have same tags as uikit exclusions',
+  test => {
+    //arrange
+    const uikit = { excludedTags: ['bat-tag'] };
+    const pattern = { tags: 'foo-tag' };
+
+    //act
+    const result = uikitExcludePattern(pattern, uikit);
+
+    //assert
+    test.false(result);
+    test.end();
+  }
+);
+
+tap.test(
+  'uikitExcludePattern - returns true when uikit has same tags as pattern',
+  test => {
+    //arrange
+    const uikit = { excludedTags: ['bar-tag', 'foo-tag'] };
+    const pattern = { tags: 'foo-tag' };
+
+    //act
+    const result = uikitExcludePattern(pattern, uikit);
+
+    //assert
+    test.true(result);
+    test.end();
+  }
 );
