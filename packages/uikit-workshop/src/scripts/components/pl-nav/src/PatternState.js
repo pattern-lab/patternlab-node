d0155b57c13ac6afef8712912b3a0c8e0de8f661
Salem Ghoweri <salem.ghoweri@pega.com>
2020-04-25 10:52:41 -0400

    refactor: split apart existing Nav UI into smaller, more manageable pieces

---
+++ packages/uikit-workshop/src/scripts/components/pl-nav/src/PatternState.js
@@ -0,0 +1,10 @@
+import { h } from 'preact';
+
+export const PatternState = props => {
+  return (
+    <span
+      class={`pl-c-pattern-state pl-c-pattern-state--${props.variant}`}
+      title={props.variant}
+    />
+  );
+};
