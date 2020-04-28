eeefb97b5e0bf78f34e9bc047c4bbbef29ae5273
Josef Bredreck <slime.games@outlook.de>
2020-04-28 20:30:47 +0200

    Revert merge #1143: Fix naming of view-all links

---
+++ packages/uikit-workshop/src/scripts/components/pl-nav/src/NavLink.js
@@ -0,0 +1,26 @@
+import { h } from 'preact';
+import { PatternState } from './PatternState';
+
+export const NavLink = props => {
+  return (
+    <a
+      href={`patterns/${props.item.patternPath}`}
+      className={`pl-c-nav__link pl-c-nav__link--sublink
+            ${
+              props.item.isDocPattern
+                ? 'pl-c-nav__link--overview'
+                : 'pl-c-nav__link--subsublink'
+            }
+          `}
+      onClick={e => props.elem.handleClick(e, props.item.patternPartial)}
+      data-patternpartial={props.item.patternPartial}
+    >
+      {props.item.isDocPattern && props.category
+        ? `${props.category}`
+        : props.item.patternName}
+      {props.item.patternState && (
+        <PatternState variant={props.item.patternState} />
+      )}
+    </a>
+  );
+};
