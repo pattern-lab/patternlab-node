299f9e1f48b198c8d4ff127ab1553fe7e7a7f164
Josef Bredreck <slime.games@outlook.de>
2020-04-29 18:31:47 +0200

    Add highlighting for typescript
    Removed 'hbs' again because it looks worse than the natural markdown highlighting

--- packages/uikit-workshop/src/scripts/components/prism-languages.js
+++ packages/uikit-workshop/src/scripts/components/prism-languages.js
@@ -4,6 +4,7 @@ import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-twig';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-typescript';
 import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-css-extras';
@@ -12,10 +13,5 @@ import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-markdown';
 import 'prismjs/components/prism-yaml';
 import 'prismjs/components/prism-handlebars';
-
-if (!Prism.languages.hasOwnProperty('hbs')) {
-  // hbs doesn't exist initially
-  Prism.languages.hbs = Prism.languages.handlebars;
-}
 
 export const PrismLanguages = Prism;
