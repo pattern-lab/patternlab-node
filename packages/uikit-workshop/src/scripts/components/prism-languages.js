7351cc351d47bd4187a23989c01357672dcf418f
Josef Bredreck <slime.games@outlook.de>
2020-04-29 17:55:17 +0200

    Add highlighting for hbs and json

--- packages/uikit-workshop/src/scripts/components/prism-languages.js
+++ packages/uikit-workshop/src/scripts/components/prism-languages.js
@@ -4,6 +4,7 @@ import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-twig';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-css-extras';
 import 'prismjs/components/prism-scss';
@@ -11,5 +12,10 @@ import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-markdown';
 import 'prismjs/components/prism-yaml';
 import 'prismjs/components/prism-handlebars';
+
+if (!Prism.languages.hasOwnProperty('hbs')) {
+  // hbs doesn't exist initially
+  Prism.languages.hbs = Prism.languages.handlebars;
+}
 
 export const PrismLanguages = Prism;
