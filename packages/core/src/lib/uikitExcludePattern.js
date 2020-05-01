7889028bb02f284b6bd8b70ed35351c79f297ccc
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2020-05-01 07:14:41 -0500

    Update packages/core/src/lib/uikitExcludePattern.js
    Co-authored-by: Josef Bredreck <slime.games@outlook.de>

--- packages/core/src/lib/uikitExcludePattern.js
+++ packages/core/src/lib/uikitExcludePattern.js
@@ -6,6 +6,9 @@ const uikitExcludePattern = (pattern, uikit) => {
   const state = pattern.patternState;
   const tags = pattern.tags;
 
-  return uikit.excludedPatternStates.includes(state) || _.intersection(uikit.excludedTags, tags).length > 0;
+  return (
+    _.includes(uikit.excludedPatternStates, state) ||
+    _.intersection(uikit.excludedTags, tags).length > 0
+  );
 };
 module.exports = uikitExcludePattern;
