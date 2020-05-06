b0a880acbecc03151c1d07bd3d7b9def4f5f2a79
Josef Bredreck <slime.games@outlook.de>
2020-05-06 17:22:51 +0200

    #1199: Fix merge patterns when needed

--- packages/core/src/lib/pseudopattern_hunter.js
+++ packages/core/src/lib/pseudopattern_hunter.js
@@ -67,9 +67,9 @@ pseudopattern_hunter.prototype.find_pseudopatterns = function(
         (objValue, srcValue) => {
           if (
             _.isArray(objValue) &&
-            patternlab.config.patternMergeVariantArrays
+            !patternlab.config.patternMergeVariantArrays
           ) {
-            return objValue.concat(srcValue);
+            return srcValue;
           }
           // Lodash will only check for "undefined" and eslint needs a consistent
           // return so do not remove
