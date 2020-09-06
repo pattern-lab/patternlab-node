1b6ff9f1ef5be59c8c562f48ec144fa1f3ae9104
Ofer Shaal <shaal@palantir.net>
2020-09-06 08:35:20 -0400

    Fix IE11 errors and polyfills (#1255)
    * Fix IE11 error - SCRIPT438: Object doesn't support property or method 'forEach'
    * Update uikit polyfills to run pl in ie11
    * Update packages/uikit-workshop/src/scripts/components/modal-styleguide.js
    * Update modal-styleguide.js
    
    Co-authored-by: Ofer Shaal <ofer@3paces.com> Josef Bredreck <slime.games@outlook.de>

--- packages/uikit-workshop/src/scripts/patternlab-pattern.js
+++ packages/uikit-workshop/src/scripts/patternlab-pattern.js
@@ -1,9 +1,4 @@
 import 'regenerator-runtime/runtime';
-if (window.NodeList && !NodeList.prototype.forEach) {
-  NodeList.prototype.forEach = Array.prototype.forEach;
-}
-import '@pattern-lab/uikit-polyfills/platform/custom-event';
-import '@pattern-lab/uikit-polyfills/platform/symbol';
 import './components/modal-styleguide';
 import './components/pl-search/pl-search.iframe-helper';
 import './components/pl-nav/pl-nav.iframe-helper';
