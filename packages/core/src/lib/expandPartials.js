b52e833ea8e5370557d8e41fa5f6bffe1fea4101
Maximilian <github-public-address-20201011-mfr@nzke.net>
2021-03-28 14:59:24 +0200

    test(lint): integrated code formatting feedback

--- packages/core/src/lib/expandPartials.js
+++ packages/core/src/lib/expandPartials.js
@@ -20,7 +20,7 @@ module.exports = function(currentPattern, patternlab) {
   // results
   if (
     currentPattern.engine.expandPartials &&
-    (foundPatternPartials !== null && foundPatternPartials.length > 0)
+    foundPatternPartials !== null && foundPatternPartials.length > 0
   ) {
     logger.debug(`found partials for ${currentPattern.patternPartial}`);
 
