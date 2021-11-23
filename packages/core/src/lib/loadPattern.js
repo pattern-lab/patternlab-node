2cfd0229eef47f582ff0baa17ad5aa28f8947e14
Maximilian <787658+mfranzke@users.noreply.github.com>
2021-11-23 23:15:53 +0100

    chore: code optimizations
    that are mentioned by the github pipeline as well

--- packages/core/src/lib/loadPattern.js
+++ packages/core/src/lib/loadPattern.js
@@ -133,10 +133,12 @@ module.exports = function (relPath, patternlab) {
   currentPattern.template = fs.readFileSync(templatePath, 'utf8');
 
   //find any stylemodifiers that may be in the current pattern
-  currentPattern.stylePartials = currentPattern.findPartialsWithStyleModifiers();
+  currentPattern.stylePartials =
+    currentPattern.findPartialsWithStyleModifiers();
 
   //find any pattern parameters that may be in the current pattern
-  currentPattern.parameteredPartials = currentPattern.findPartialsWithPatternParameters();
+  currentPattern.parameteredPartials =
+    currentPattern.findPartialsWithPatternParameters();
 
   [
     templatePath,
