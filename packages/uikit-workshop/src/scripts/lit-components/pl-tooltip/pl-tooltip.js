4527e4ad1f9a9cb50e9ff0008b36ad8805bb5a62
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2021-12-25 10:41:47 +0100

    refactor: fixed "warning  'customElement' is defined but never used  no-unused-vars"

--- packages/uikit-workshop/src/scripts/lit-components/pl-tooltip/pl-tooltip.js
+++ packages/uikit-workshop/src/scripts/lit-components/pl-tooltip/pl-tooltip.js
@@ -1,4 +1,4 @@
-import { LitElement, html, customElement } from 'lit-element';
+import { LitElement, html } from 'lit-element';
 import styles from './pl-tooltip.scss?external';
 import { Slotify } from '../slotify';
 
