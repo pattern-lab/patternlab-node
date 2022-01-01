97f196469cf205c0285a0bea34f13f51c060951e
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2022-01-01 15:01:44 +0100

    refactor: corrected some aspects out of eslint feedback (#1399)
    * refactor: fixed 'warning  'data' is assigned a value but never used  no-unused-vars'
    
    its usage has been removed with https://github.com/pattern-lab/patternlab-node/commit/70fe9b63676e21439ba77d5ddbdbc5b243ba829f#diff-75bedd8b06cebb50236c7c659b539b0fa984746f8abb3d7a5dc6a92b3763fcc0
    
    * refactor: fixed "warning  'event' is defined but never used          no-unused-vars"
    
    * refactor: fixed "warning  'NodePart' is defined but never used                 no-unused-vars"
    
    This isn't in use anymore since commit https://github.com/pattern-lab/patternlab-node/commit/2ff8e1c98cdd02e8077064c48eca5f7754a3db02#diff-b26f09ed0fc0febb6d869ff60e410d9d8efd300b4b6c2facbf668cd4555e27a2L39
    
    * refactor: fixed "warning  'customElement' is defined but never used  no-unused-vars"
    
    * refactor: fixed "warning  'previousValues' is assigned a value but never used  no-unused-vars"
    
    as its usage hasn't been in use since commit https://github.com/pattern-lab/patternlab-node/commit/2ff8e1c98cdd02e8077064c48eca5f7754a3db02#diff-b26f09ed0fc0febb6d869ff60e410d9d8efd300b4b6c2facbf668cd4555e27a2R45
    
    * refactor: fixed "error  Method 'slotify' expected a return value  consistent-return"
    
    * refactor: fixed "warning  'unsafeCSS' is defined but never used  no-unused-vars"
    
    and css and svg as well
    
    * refactor: fixed "warning  'event' is defined but never used  no-unused-vars"
    
    * refactor: fixed "warning  'event' is defined but never used     no-unused-vars"
    
    and filePath as well
    
    * refactor: fixed "warning  'pm' is assigned a value but never used  no-unused-vars"
    
    as its usage has been removed with commit https://github.com/pattern-lab/patternlab-node/commit/deb4783140fb719057cb7ab11ce99d12f2e9e061#diff-25426a917b53575c0f0efc5674ee70c4d2c8671f9ade4f9d994db43c9552302bL217
    
    * refactor: fixed "warning  'options' is defined but never used  no-unused-vars"
    
    * chore: removing the previously out-commented parts

--- packages/cli/bin/cli-actions/init.js
+++ packages/cli/bin/cli-actions/init.js
@@ -14,7 +14,7 @@ const writeJsonAsync = require('../utils').writeJsonAsync;
 const defaultPatternlabConfig = patternlab.getDefaultConfig();
 
 // https://github.com/TehShrike/deepmerge#overwrite-array
-const overwriteMerge = (destinationArray, sourceArray, options) => sourceArray;
+const overwriteMerge = (destinationArray, sourceArray) => sourceArray;
 
 const init = (options) =>
   wrapAsync(function* () {
