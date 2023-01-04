2bf8b8a395cea9e5dab30f74ccfedd9e2ac37767
Maximilian <787658+mfranzke@users.noreply.github.com>
2023-01-04 13:17:16 +0100

    chore(cli): optimized logging output
    removed some leftovers from https://github.com/pattern-lab/patternlab-node/commit/4a5c199e74c7fcfe5c03e9e5cd277a9ffc9cb79f changes, the old console.log method endings

--- packages/cli/bin/cli-actions/help.js
+++ packages/cli/bin/cli-actions/help.js
@@ -3,8 +3,8 @@ module.exports = () => {
   /* eslint-disable */
   console.log(`
   Examples:
-    $ patternlab init # Initialize a Pattern Lab project.');
-    $ patternlab <cmd> # Builds Pattern Lab from the current dir');
-    $ patternlab <cmd> --config <path/to/patternlab-config> # Pattern Lab from a config in a specified directory');`);
+    $ patternlab init # Initialize a Pattern Lab project
+    $ patternlab <cmd> # Builds Pattern Lab from the current dir
+    $ patternlab <cmd> --config <path/to/patternlab-config> # Pattern Lab from a config in a specified directory`);
   /* eslint-enable */
 };
