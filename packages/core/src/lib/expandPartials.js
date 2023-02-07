b94b89209c2c47f6c86bbedb62e5f6097d38d94a
Josef Bredreck <slime.games@outlook.de>
2023-02-07 17:10:06 +0100

    fix: issue in mustache runable

--- packages/core/src/lib/expandPartials.js
+++ packages/core/src/lib/expandPartials.js
@@ -4,6 +4,7 @@ const logger = require('./log');
 const jsonCopy = require('./json_copy');
 const getPartial = require('./get');
 
+// TODO: remove when removing mustache
 module.exports = function (currentPattern, patternlab) {
   const processRecursive = require('./processRecursive');
 
@@ -19,6 +20,47 @@ module.exports = function (currentPattern, patternlab) {
     foundPatternPartials.length > 0
   ) {
     logger.debug(`found partials for ${currentPattern.patternPartial}`);
+
+    //do something with the regular old partials
+    foundPatternPartials.forEach((foundPartial) => {
+      const partial = currentPattern.findPartial(foundPartial);
+      const partialPattern = getPartial(partial, patternlab);
+
+      //recurse through nested partials to fill out this extended template.
+      return processRecursive(partialPattern.relPath, patternlab)
+        .then(() => {
+          //eslint-disable-line no-loop-func
+
+          //complete assembly of extended template
+          //create a copy of the partial so as to not pollute it after the getPartial call.
+          const cleanPartialPattern = jsonCopy(
+            partialPattern,
+            `partial pattern ${partial}`
+          );
+
+          //this is what we came here for
+          logger.debug(
+            `within ${currentPattern.patternPartial}, replacing extendedTemplate partial ${foundPartial} with ${cleanPartialPattern.patternPartial}'s extendedTemplate`
+          );
+
+          currentPattern.extendedTemplate =
+            currentPattern.extendedTemplate.replace(
+              foundPartial,
+              cleanPartialPattern.extendedTemplate
+            );
+
+          // update the extendedTemplate in the partials object in case this
+          // pattern is consumed later
+          patternlab.partials[currentPattern.patternPartial] =
+            currentPattern.extendedTemplate;
+
+          return Promise.resolve();
+        })
+        .catch((reason) => {
+          console.log(reason);
+          logger.error(reason);
+        });
+    });
   }
   return Promise.resolve();
 };
