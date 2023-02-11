11b196816dd212297c1368567d48dd6915297b30
Josef Bredreck <13408112+JosefBredereck@users.noreply.github.com>
2023-02-11 23:10:46 +0100

    fix: build scripts

--- packages/docs/tools/rename-output.js
+++ packages/docs/tools/rename-output.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const { resolve } = require('path');
 
-const distPath = resolve(process.cwd(), 'dist');
-const outputPath = resolve(process.cwd(), 'output');
+const distPath = resolve(__dirname, '..', 'dist');
+const outputPath = resolve(__dirname, '..', 'output');
 
 if (fs.existsSync(outputPath)) {
   fs.rmSync(outputPath, { recursive: true, force: true });
